149042c143d621b4cbb9bcbb29cc98c9fcd71173
Ali Abrar <aliabrar@gmail.com>
2015-04-07 21:58:57 -0400

    bump reflex and reflex-dom, no longer use forked ghcjs-dom

--- default.nix
+++ default.nix
@@ -9,13 +9,6 @@ let overrideCabal = drv: f: (drv.override (args: args // {
         reflex = self.callPackage ./reflex {};
         reflex-dom = self.callPackage ./reflex-dom {};
         reflex-todomvc = self.callPackage ./reflex-todomvc {};
-        ghcjs-dom = overrideCabal super.ghcjs-dom (drv: {
-          src = nixpkgs.fetchgit {
-            url = git://github.com/xionite/ghcjs-dom.git;
-            rev = "2d5a0aa64454f2c084b1a7c53cadb59b274d1386";
-            sha256 = "8450b1a0de67cf6bb6c304c244e211331da8f5befdf92c089498c4394c14fcc2";
-          };
-        });
       };
     };
 in rec {
