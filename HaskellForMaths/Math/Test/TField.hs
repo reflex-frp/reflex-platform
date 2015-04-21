62efee957515d170e4bce6f015ae3c053d43a519
Ryan Trinkle <ryan.trinkle@gmail.com>
2015-04-21 03:07:18 -0400

    Remove HaskellForMaths

--- HaskellForMaths/Math/Test/TField.hs
+++
@@ -1,9 +0,0 @@
-module Math.Test.TField where
-
-import Math.Algebra.Field.Base
-import Math.Algebra.Field.Extension
-
-test = and
-    [ (1/5 :: QSqrt3) * 5 == 1 -- regression test for defect
-    , (1/4 :: F25) * 4 == 1
-    ]
