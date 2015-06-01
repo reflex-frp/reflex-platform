d36b13b7220253bcac1d6631e5a533049a518143
John Ericson <Ericson2314@Yahoo.com>
2015-06-01 08:32:10 +0000

    Separate the demo from the core deps

--- default.nix
+++ default.nix
@@ -1,59 +1,12 @@
-{ system ? null }:
-let overrideCabal = drv: f: (drv.override (args: args // {
-      mkDerivation = drv: args.mkDerivation (drv // f drv);
-    })) // {
-      overrideScope = scope: overrideCabal (drv.overrideScope scope) f;
-    };
-    nixpkgs = import ./nixpkgs ({
-      config.allowUnfree = true;
-    } // (if system == null then {} else { inherit system; }));
-    extendHaskellPackages = haskellPackages: haskellPackages.override {
-      overrides = self: super: {
-        reflex = self.callPackage ./reflex {
-          inherit (super) reflex;
-          inherit overrideCabal;
-        };
-        reflex-dom = self.callPackage ./reflex-dom {
-          inherit (super) reflex-dom;
-          inherit (nixpkgs) fetchFromGitHub;
-        };
-        reflex-todomvc = self.callPackage ./reflex-todomvc {
-          inherit (super) reflex-todomvc;
-          inherit (nixpkgs) fetchFromGitHub;
-        };
-        active = overrideCabal super.active (drv: {
-          version = "0.1.0.19";
-          sha256 = "1zzzrjpfwxzf0zbz8vcnpfqi7djvrfxglhkvw1s6yj5gcblg2rcw";
-          doCheck = false;
-        });
-        thyme = overrideCabal super.thyme (drv: {
-          doCheck = false;
-        });
-        orgmode-parse = overrideCabal super.orgmode-parse (with self; drv: {
-          version = "0.1.0.4";
-          sha256 = "098zl8nyph459zyla0y2mkqiy78zp74yzadrnwa6xv07i5zs125h";
-          buildDepends = [
-            aeson attoparsec free hashable text thyme unordered-containers
-          ];
-          testDepends = [
-            aeson attoparsec hashable HUnit tasty tasty-hunit text thyme
-            unordered-containers
-          ];
-          doCheck = false;
-        });
-        HaskellForMaths = overrideCabal super.HaskellForMaths (drv: {
-          version = "0.4.8";
-          sha256 = "0yn2nj6irmj24j1djvnnq26i2lbf9g9x1wdhmcrk519glcn5k64j";
-          buildDepends = [ self.semigroups ] ++ drv.buildDepends; # For some reason, without the spurious import of self.semigroups, HaskellForMaths will fail to build the environment for HaskellForMaths on ghcjs (it works on ghc)
-        });
-      };
-    };
-in rec {
-  inherit nixpkgs;
-  ghc = extendHaskellPackages nixpkgs.pkgs.haskell-ng.packages.ghc7101;
-  ghcjs = extendHaskellPackages nixpkgs.pkgs.haskell-ng.packages.ghcjs;
-  platforms = [ "ghcjs" ] ++ (if !nixpkgs.stdenv.isDarwin then [ "ghc" ] else []);
+{ nixpkgs, haskellPackages, platform }:
 
+haskellPackages.mkDerivation {
+  pname = "reflex-demo";
+  version = "0.0.0.0";
+  license = null;
+
+  buildDepends =  import ./packages.nix { inherit haskellPackages platform; };
+} // {
   # The systems that we want to build for on the current system
   cacheTargetSystems =
     if nixpkgs.stdenv.system == "x86_64-linux"
--- shell.nix
+++ shell.nix
@@ -1,13 +1,25 @@
 { system ? null }:
-let this = import ./. { inherit system; };
-    reflexEnv = platform: (builtins.getAttr platform this).ghcWithPackages (p: import ./packages.nix { haskellPackages = p; inherit platform; });
-in this.nixpkgs.runCommand "shell" {
+
+let
+  nixpkgs = import ./deps { inherit system; };
+
+  hsPkgSet = [ { haskellPackages = nixpkgs.haskell-ng.packages.ghcjs;
+                 platform = "ghcjs";
+               }]
+             ++ (if !nixpkgs.stdenv.isDarwin
+                 then [ { haskellPackages = nixpkgs.haskell-ng.packages.ghc7101;
+                          platform =  "ghc"; } ]
+                 else []);
+
+  versions = builtins.map (args: import ./. ({ inherit nixpkgs; } // args)) hsPkgSet;
+
+in nixpkgs.runCommand "shell" {
   buildCommand = ''
     echo "$propagatedBuildInputs $buildInputs $nativeBuildInputs $propagatedNativeBuildInputs" > $out
   '';
   buildInputs = [
-    this.nixpkgs.nodejs
-    this.nixpkgs.curl
-    this.ghc.cabal-install
-  ] ++ builtins.map reflexEnv this.platforms;
+    nixpkgs.nodejs
+    nixpkgs.curl
+    nixpkgs.haskellPackages.cabal-install
+  ] ++ nixpkgs.stdenv.lib.concatMap (p: p.env.nativeBuildInputs) versions;
 } ""
