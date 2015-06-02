5b4a47fcb314446f287eca7b19c77bb9e0f36ac8
John Ericson <Ericson2314@Yahoo.com>
2015-06-02 06:22:01 +0000

    Fix some scripts

--- default.nix
+++ default.nix
@@ -6,10 +6,4 @@ haskellPackages.mkDerivation {
   license = null;
 
   buildDepends =  import ./packages.nix { inherit haskellPackages platform; };
-} // {
-  # The systems that we want to build for on the current system
-  cacheTargetSystems =
-    if nixpkgs.stdenv.system == "x86_64-linux"
-    then [ "x86_64-linux" "i686-linux" ] # On linux, we want to build both 32-bit and 64-bit versions
-    else [ nixpkgs.stdenv.system ];
 }
