b9076b91f0e2065a682320e9ae02f07e70e06cd3
John Ericson <Ericson2314@Yahoo.com>
2015-06-18 20:43:12 -0700

    No more submodules! Use dynamic imports instead

---
+++ reflex/default.nix
@@ -0,0 +1,6 @@
+import ((import <nixpkgs> {}).fetchFromGitHub {
+  owner = "ryantrinkle";
+  repo = "reflex";
+  rev = "2653f40d0ec8ef1d0372e941a8bcde991fd79979";
+  sha256 = "0afm88flwynsnna60gmpanma3mm7id5qq2bmxl9c0f9gc55msn76";
+})
