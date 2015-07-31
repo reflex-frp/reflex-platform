12f8a4434bb7cfcce8551f030c9b3a4e749e893f
Ryan Trinkle <ryan.trinkle@gmail.com>
2015-07-31 18:06:05 -0400

    Add ./release

--- default.nix
+++ default.nix
@@ -99,6 +99,56 @@ in rec {
   ghcjs = extendHaskellPackages nixpkgs.pkgs.haskell-ng.packages.ghcjs;
   platforms = [ "ghcjs" ] ++ (if !nixpkgs.stdenv.isDarwin then [ "ghc" ] else []);
 
+  attrsToList = s: map (name: { inherit name; value = builtins.getAttr name s; }) (builtins.attrNames s);
+  mapSet = f: s: builtins.listToAttrs (map ({name, value}: {
+    inherit name;
+    value = f value;
+  }) (attrsToList s));
+  mkSdist = pkg: pkg.override {
+    mkDerivation = drv: ghc.mkDerivation (drv // {
+      postConfigure = ''
+        ./Setup sdist
+        mkdir "$out"
+        mv dist/*.tar.gz "$out/${drv.pname}-${drv.version}.tar.gz"
+        exit 0
+      '';
+    });
+  };
+  sdists = mapSet mkSdist ghc;
+  mkHackageDocs = pkg: pkg.override {
+    mkDerivation = drv: ghc.mkDerivation (drv // {
+      postConfigure = ''
+        ./Setup haddock --hoogle --hyperlink-source --html --html-location='/package/${drv.pname}-${drv.version}/docs' --contents-location='/package/${drv.pname}-${drv.version}' --haddock-option=--built-in-themes
+        cd dist/doc/html
+        mv "${drv.pname}" "${drv.pname}-${drv.version}-docs"
+        mkdir "$out"
+        tar cz --format=ustar -f "$out/${drv.pname}-${drv.version}-docs.tar.gz" "${drv.pname}-${drv.version}-docs"
+        exit 0
+      '';
+    });
+  };
+  hackageDocs = mapSet mkHackageDocs ghc;
+  mkReleaseCandidate = pkg: nixpkgs.stdenv.mkDerivation (rec {
+    name = pkg.name + "-rc";
+    sdist = mkSdist pkg + "/${pkg.pname}-${pkg.version}.tar.gz";
+    docs = mkHackageDocs pkg + "/${pkg.pname}-${pkg.version}-docs.tar.gz";
+
+    builder = builtins.toFile "builder.sh" ''
+      source $stdenv/setup
+
+      mkdir "$out"
+      echo -n "${pkg.pname}-${pkg.version}" >"$out/pkgname"
+      ln -s "$sdist" "$docs" "$out"
+    '';
+
+    # 'checked' isn't used, but it is here so that the build will fail if tests fail
+    checked = overrideCabal pkg (drv: {
+      doCheck = true;
+      src = sdist;
+    });
+  });
+  releaseCandidates = mapSet mkReleaseCandidate ghc;
+
   # The systems that we want to build for on the current system
   cacheTargetSystems =
     if nixpkgs.stdenv.system == "x86_64-linux"
