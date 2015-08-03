db5403bdc5d81b1aebbcc0dd5f27c1813a7816a4
Ryan Trinkle <ryan.trinkle@gmail.com>
2015-08-02 20:28:46 -0400

    Revert "Disable most packages, so that we can begin testing ghcjs on improved-base"
    This reverts commit ebda6aabba8ccc3bff38e249d585c789f1f1f4a8.
    
    # Conflicts:
    #	packages.nix

--- packages.nix
+++ packages.nix
@@ -6,7 +6,10 @@ with haskellPackages;
   ##############################################################################
   # Add general packages here                                                  #
   ##############################################################################
-reflex
+  reflex
+  reflex-dom
+  reflex-todomvc
+
 ] ++ (if platform == "ghcjs" then [
   ##############################################################################
   # Add ghcjs-only packages here                                               #
@@ -17,4 +20,4 @@ reflex
   # Add ghc-only packages here                                                 #
   ##############################################################################
 
-] else [])
+] else []) ++ builtins.concatLists (map (x: x.override { mkDerivation = drv: drv.buildDepends; }) [ reflex reflex-dom reflex-todomvc ])
