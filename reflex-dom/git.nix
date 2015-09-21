ec7c8f3d4235cc0ed626b3563fea507090eba2d3
Ali Abrar <aliabrar@gmail.com>
2015-09-20 20:57:33 -0400

    Bump reflex-dom

--- reflex-dom/git.nix
+++ reflex-dom/git.nix
@@ -1,5 +1,5 @@
 {
   url = git://github.com/ryantrinkle/reflex-dom;
-  rev = "b8372b875ff89e32d788739cc9833671fdb5cf53";
-  sha256 = "7076138305e1192273c867446bea30c474da1d95ea23a185e993b73d3971b3d6";
+  rev = "c61c99af7212effeeba52b39588dc4aa2d3eaa92";
+  sha256 = "2016d188aabc756863b1ca6d00115b7ccd237c719facfbb975e48ddd84629bdc";
 }
