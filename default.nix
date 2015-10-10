bb91946b3319f4912c63e5f128a6f2378e811ae0
Ryan Trinkle <ryan.trinkle@gmail.com>
2015-10-09 21:30:10 -0400

    Refactor work-on to put more of the functionality into default.nix

--- default.nix
+++ default.nix
@@ -285,9 +285,15 @@ in rec {
   });
   releaseCandidates = mapSet mkReleaseCandidate ghc;
 
+  workOn = package: (overrideCabal package (drv: {
+    buildDepends = drv.buildDepends ++ [ ghc.cabal-install ghc.ghcid ];
+  })).env;
+
   # The systems that we want to build for on the current system
   cacheTargetSystems =
     if nixpkgs.stdenv.system == "x86_64-linux"
     then [ "x86_64-linux" "i686-linux" ] # On linux, we want to build both 32-bit and 64-bit versions
     else [ nixpkgs.stdenv.system ];
+
+  inherit lib;
 }
