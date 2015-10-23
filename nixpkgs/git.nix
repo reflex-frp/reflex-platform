debd53ac24cea0a51e72db8fa43706aff363eec0
Ryan Trinkle <ryan.trinkle@gmail.com>
2015-10-23 10:42:03 -0400

    Bump nixpkgs

--- nixpkgs/git.nix
+++ nixpkgs/git.nix
@@ -1,5 +1,5 @@
 {
   url = git://github.com/ryantrinkle/nixpkgs;
-  rev = "1c8e3a43837a3f21f017ea94a1f712af381d8edb";
-  sha256 = "853f5e88101db55ad76c369700e4505d0b38c31719bd4ed65dae97740cdc64d8";
+  rev = "1ea76c92c0d35fee2fa35e7b992525cd72918aa4";
+  sha256 = "b9a5e6b31daa3c12f9156ba2bd26d6e62eac579f8640d109e53a68d6015b3e46";
 }
