6747124f23c6171e2dc570410691320680274ee7
Ryan Trinkle <ryan.trinkle@gmail.com>
2015-10-31 17:48:10 -0400

    Bump reflex-dom

--- reflex-dom/git.nix
+++ reflex-dom/git.nix
@@ -1,5 +1,5 @@
 {
   url = git://github.com/ryantrinkle/reflex-dom;
-  rev = "9cfb7d913671f9142f78a25de4184759f54b01e0";
-  sha256 = "f463fc469a4cd0737a911d853062ad47d0449aa9fe904d4cd4986d18a0870062";
+  rev = "d778094352f99c5fa59770710eb6accb141550eb";
+  sha256 = "9205f81f763d17733b104dd9b680d3e01362aaf5c1aaf965f22b36f8476b3c15";
 }
