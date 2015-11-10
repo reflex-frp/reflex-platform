e0b4c795f6d5796960fb6996bdea0804ab01b38c
Ryan Trinkle <ryan.trinkle@gmail.com>
2015-11-10 04:35:39 -0500

    Fix nixpkgs URL

--- nixpkgs/git.nix
+++ nixpkgs/git.nix
@@ -1,5 +1,5 @@
 {
-  url = git://github.com/ryantrinkle/nixpkgs;
+  url = git://github.com/NixOS/nixpkgs;
   rev = "60b4d47f90cb872ac4552be049ebdf8dc4791191";
   sha256 = "b669ea7639ab3c90a8998a5831e778604be0c7afe9a4e2bbd175ef594fb86386";
 }
