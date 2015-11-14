4f2c2ee367abc372e70d68773899fa54eee68c7e
Rommel M. Martinez <ebzzry@gmail.com>
2015-11-15 03:37:49 +0800

    Updated shell scripts.

--- common-setup.sh
+++ common-setup.sh
@@ -1,57 +1,100 @@
-# This file should not be run directly; it exists to share setup code between the various scripts in try-reflex
-# Before running this script, DIR should be defined equal to the directory containing this script
+# This file should not be run directly; it exists to share setup code
+# between the various scripts in try-reflex. Before running this
+# script, DIR should be defined equal to the directory containing this
+# script
 
+# The primary source repository of try-reflex
 REPO="https://github.com/ryantrinkle/try-reflex"
 
+# These are options passed to nix-instantiate and nix-shell.
 NIXOPTS="--option extra-binary-caches https://ryantrinkle.com:5443/ -j 8"
+
+# The minimum required version of Nix to run this script.
+MIN_NIX_VERSION="1.8"
+
+# Whether the nix script needed to be sourced - i.e. nix commands are
+# not available without doing so, from the user's basic prompt.
+NEEDED_TO_SOURCE_NIX_SCRIPT=0
 
 trap "echo 'It looks like a problem occurred.  Please submit an issue at $REPO/issues'; exit 1" ERR
 
-echo "If you have any trouble with this script, please submit an issue at $REPO/issues"
+displayMessage() {
+    cat <<EOF
+If you have any trouble with this script, please submit an
+issue at $REPO/issues
+EOF
+}
 
+ensureNix() {
 (
+    cd "$DIR"
 
-cd "$DIR"
+    if [[ ! -d /nix ]] ; then
+        if ! type -P curl >/dev/null ; then
+            echo "Please make sure that 'curl' is installed and can be run from this shell"
+            exit 1
+        fi
 
-if [ ! -d /nix ] ; then
-  if ! type -P curl >/dev/null ; then
-    echo "Please make sure that 'curl' is installed and can be run from this shell"
-    exit 1
-  fi
-
-  echo "In order to continue, $0 must install the Nix package manager.  This requires root access, so you will be prompted for your password.  If you do not wish to continue, just hit Ctrl-C at the password prompt."
-  ./installNix.sh
-fi
-
+        cat <<EOF
+In order to continue, $0 must install the Nix package manager.  This
+requires root access, so you will be prompted for your password.  If
+you do not wish to continue, just hit Ctrl-C at the password prompt.
+EOF
+        ./installNix.sh
+    fi
 )
+}
 
-# The command to source the nix script.  This should be a line of valid bash code.
-SOURCE_NIX_SCRIPT=". $HOME/.nix-profile/etc/profile.d/nix.sh"
+sourceNixScript() {
+    source $HOME/.nix-profile/etc/profile.d/nix.sh
+}
 
-# Whether the nix script needed to be sourced - i.e. nix commands are not available without doing so, from the user's basic prompt.
-NEEDED_TO_SOURCE_NIX_SCRIPT=0
+checkNixShell() {
+    if ! type -P nix-shell >/dev/null ; then
+        sourceNixScript
+        NEEDED_TO_SOURCE_NIX_SCRIPT=1
 
-if ! type -P nix-shell >/dev/null ; then
-  $SOURCE_NIX_SCRIPT
-  NEEDED_TO_SOURCE_NIX_SCRIPT=1
-  if ! type -P nix-shell >/dev/null ; then
-    echo "It looks like Nix isn't working.  Please make sure you can run nix-shell, then retry the $0, or submit an issue at $REPO/issues"
-    exit 1
-  fi
-fi
+        if ! type -P nix-shell >/dev/null ; then
+            cat <<EOF
+It looks like Nix isn't working.  Please make sure you can run
+nix-shell, then retry the $0, or submit an issue at
+$REPO/issues"
+EOF
+            exit 1
+        fi
+    fi
+}
 
-# The minimum required version of Nix to run this script.
-MIN_REQUIRED_NIX_VERSION="1.8"
+checkNixVersion() {
+    if [[ "$(nix-instantiate --eval --expr "builtins.compareVersions builtins.nixVersion \"$MIN_NIX_VERSION\" >= 0")" != "true" ]] ; then
+        cat <<EOF
+It looks like your version of Nix, $(nix-instantiate --eval --expr "builtins.nixVersion"),
+is older than the minimum version required by try-reflex,
+\"$MIN_NIX_VERSION\".  You'll need to upgrade Nix to continue.  On
+non-NixOS platforms, that can usually be done like this:
+EOF
+        if [[ "$NEEDED_TO_SOURCE_NIX_SCRIPT" -ne 0 ]] ; then
+            echo "source $HOME/.nix-profile/etc/profile.d/nix.sh"
+        fi
 
-if [ "$(nix-instantiate --eval --expr "builtins.compareVersions builtins.nixVersion \"$MIN_REQUIRED_NIX_VERSION\" >= 0")" != "true" ] ; then
-  echo "It looks like your version of Nix, $(nix-instantiate --eval --expr "builtins.nixVersion"), is older than the minimum version required by try-reflex, \"$MIN_REQUIRED_NIX_VERSION\".  You'll need to upgrade Nix to continue.  On non-NixOS platforms, that can usually be done like this:"
-  if [ "$NEEDED_TO_SOURCE_NIX_SCRIPT" -ne 0 ] ; then
-    echo "$SOURCE_NIX_SCRIPT"
-  fi
-  echo "nix-env --upgrade"
-  echo "If you're on NixOS, you may need to upgrade your OS to a later version.  See https://nixos.org/nixos/manual/sec-upgrading.html"
-  exit 1
-fi
+        echo "    nix-env --upgrade"
+
+        cat <<EOF
+If you're on NixOS, you may need to upgrade your OS to a later
+version:
+
+    nixos-rebuild switch --upgrade
+
+See https://nixos.org/nixos/manual/sec-upgrading.html for more
+information
+
+If your Nix version is too old too evaluate Nixpkgs, execute:
+
+    curl -L https://hydra.nixos.org/job/nixos/trunk-combined/nixpkgs.nix.x86_64-linux/latest-finished/nix/pkg/nix-1.10-x86_64-linux.nixpkg | nix-install-package --non-interactive -
+EOF
+        exit 1
+    fi
+}
 
 git_thunk() {
     echo "import ((import <nixpkgs> {}).fetchgit (import ./git.nix))"
@@ -60,7 +103,8 @@ git_thunk() {
 git_manifest() {
     local REPO="$1"
 
-    local URL="$(git -C "$REPO" config --get remote.origin.url | sed 's_^git@github.com:_git://github.com/_')" # Don't use git@github.com origins, since these can't be accessed by nix
+    # Don't use git@github.com origins, since these can't be accessed by nix
+    local URL="$(git -C "$REPO" config --get remote.origin.url | sed 's_^git@github.com:_git://github.com/_')"
     local REV="$(git -C "$REPO" rev-parse HEAD)"
     local HASH="$($(nix-build -E "(import <nixpkgs> {}).nix-prefetch-scripts")/bin/nix-prefetch-git "$PWD/$REPO" "$REV" 2>/dev/null | tail -n 1)"
 
@@ -72,3 +116,12 @@ git_manifest() {
 }
 EOF
 }
+
+main() {
+    displayMessage
+    ensureNix
+    checkNixShell
+    checkNixVersion
+}
+
+main "$@"
