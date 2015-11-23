71fb5bf121a071299295c5863b48b90e5708889c
Rommel M. Martinez <ebzzry@gmail.com>
2015-11-23 16:50:50 +0800

    shell.nix: improved prompt readability.

--- shell.nix
+++ shell.nix
@@ -1,10 +1,12 @@
 { system ? null }:
 let this = import ./. { inherit system; };
-    reflexEnv = platform: (builtins.getAttr platform this).ghcWithPackages (p: import ./packages.nix { haskellPackages = p; inherit platform; });
+    reflexEnv = platform: (builtins.getAttr platform this).ghcWithPackages
+      (p: import ./packages.nix { haskellPackages = p; inherit platform; });
 in this.nixpkgs.runCommand "shell" {
   buildCommand = ''
     echo "$propagatedBuildInputs $buildInputs $nativeBuildInputs $propagatedNativeBuildInputs" > $out
   '';
+
   buildInputs = [
     this.nixpkgs.nodejs
     this.nixpkgs.curl
@@ -12,7 +14,8 @@ in this.nixpkgs.runCommand "shell" {
     this.ghc.ghcid
     this.ghc.cabal2nix
   ] ++ builtins.map reflexEnv this.platforms;
+
   shellHook = ''
-    export PS1="\n\[\033[1;32m\][try-reflex:\w]$\[\033[0m\] "
+    export PS1="\n\[\033[1;32m\][try-reflex:\w]\n$\[\033[0m\] "
   '';
 } ""
