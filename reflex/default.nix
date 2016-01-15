a579dc4a790825d65c150979ed3fbd4fb17b94f7
Cale Gibbard <cgibbard@gmail.com>
2016-01-14 20:39:04 -0500

    bump reflex-dom

--- reflex/default.nix
+++ reflex/default.nix
@@ -1,17 +1,1 @@
-{ mkDerivation, dependent-map, dependent-sum
-, mtl, ref-tf, semigroups, these, MemoTrie, exception-transformers
-, haskell-src-exts, haskell-src-meta
-}:
-mkDerivation {
-  pname = "reflex";
-  version = "0.4.0";
-  src = builtins.filterSource (path: type: baseNameOf path != ".git") ./.;
-  buildDepends = [
-    dependent-map dependent-sum mtl ref-tf semigroups these exception-transformers
-    haskell-src-exts haskell-src-meta
-  ];
-  testDepends = [
-    MemoTrie
-  ];
-  license = null;
-}
+import ((import <nixpkgs> {}).fetchgit (import ./git.nix))
