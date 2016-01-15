ad9d880bc24c3a595bf228db12bd4191f6efe646
Cale Gibbard <cgibbard@gmail.com>
2016-01-14 20:01:22 -0500

    Update to using dependent-sum-0.3.2.1 and dependent-map-0.2.0.1

--- reflex-dom/git.nix
+++ reflex-dom/git.nix
@@ -1,5 +1,5 @@
 {
   url = git://github.com/ryantrinkle/reflex-dom;
-  rev = "71f1e6c97037d13a80adf75649c83bffd41f8321";
-  sha256 = "fa8c0e57a72addfd55c759a48a591511ed7fe34f05513895d0c14b330283d9e3";
+  rev = "abb3b0aade014d446dffeb3e5122d107f90211ae";
+  sha256 = "a3b3cffa1c524221970d2770e8f4d3d46c0a62bec2e7aee36b32cfc1d9e62c69";
 }
