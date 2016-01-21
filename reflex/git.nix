fd16e437527310ab7cf16be8a6c51e83717de17b
Ryan Trinkle <ryan.trinkle@gmail.com>
2016-01-20 23:48:35 -0500

    Bump reflex

--- reflex/git.nix
+++ reflex/git.nix
@@ -1,5 +1,5 @@
 {
   url = git://github.com/ryantrinkle/reflex;
-  rev = "7e8415d96b02fce1bce7e2050cb41b4e2c899315";
-  sha256 = "fa55c43d54530b1da5155fdee2bf8b3733792163db0c7a6259e4684e022f477f";
+  rev = "cc62c11a6cde31412582758c236919d4bb766ada";
+  sha256 = "63059a547ef74ff842806b93c936de65c1929bd450bc148537a1e5d5db0a0832";
 }
