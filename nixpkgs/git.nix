59968d92d52ee45a40a45c5db0d76849a8680dc6
Ryan Trinkle <ryan.trinkle@gmail.com>
2016-03-15 13:43:49 -0400

    Switch to nixpkgs-channels instead of nixpkgs

--- nixpkgs/git.nix
+++ nixpkgs/git.nix
@@ -1,5 +1,5 @@
 {
-  url = git://github.com/NixOS/nixpkgs;
-  rev = "d2e0b6cf6f13691e95b94c2ef0c777c6dde1da6b";
-  sha256 = "5c5240f00384b0abb8a5fa218fd869b92883fcf1ef544e6ff06a03d38ae2c4f5";
+  url = git://github.com/NixOS/nixpkgs-channels;
+  rev = "34721289d95549d9d30fdc3d96abbcd9a5e33daf";
+  sha256 = "0sq8d7qcif2hhax5h90a5gcvp0m68l3ja4qn0fs104w7hhynwwvd";
 }
