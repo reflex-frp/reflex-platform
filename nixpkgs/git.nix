ee8d81a5b4d0504b4ca042ad66caaf157032c6d0
Ryan Trinkle <ryan.trinkle@gmail.com>
2016-03-17 00:20:34 -0400

    Update nixpkgs
    * Update hack-off to accommodate new output format for nix-prefetch-git

--- nixpkgs/git.nix
+++ nixpkgs/git.nix
@@ -1,5 +1,5 @@
 {
   url = git://github.com/NixOS/nixpkgs-channels;
-  rev = "34721289d95549d9d30fdc3d96abbcd9a5e33daf";
-  sha256 = "0sq8d7qcif2hhax5h90a5gcvp0m68l3ja4qn0fs104w7hhynwwvd";
+  rev = "cd7242d09d64669c05246a4005731b1fa1cbaa15";
+  sha256 = "1rfigiipqsnbj4grg05ch9anaa0kc1aawfic5h10h6rlpgslrrhf";
 }
