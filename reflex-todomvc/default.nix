3f06cfac4ec3e325d60b72e05998a7d898a82954
Ryan Trinkle <ryan.trinkle@gmail.com>
2016-05-11 16:39:18 -0400

    Update reflex* repositories

--- reflex-todomvc/default.nix
+++ reflex-todomvc/default.nix
@@ -1,16 +1,1 @@
-{ mkDerivation, reflex, reflex-dom, file-embed
-}:
-
-mkDerivation {
-  pname = "reflex-todomvc";
-  version = "0.1";
-  src = builtins.filterSource (path: type: baseNameOf path != ".git") ./.;
-  isExecutable = true;
-  isLibrary = true;
-  buildDepends = [
-    reflex
-    reflex-dom
-    file-embed
-  ];
-  license = null;
-}
+import ((import <nixpkgs> {}).fetchgit (builtins.fromJSON (builtins.readFile ./git.json)))
