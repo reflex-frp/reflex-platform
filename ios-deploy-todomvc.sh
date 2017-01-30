f8c5aed1ce14a7ff33b551d8538bbfc29a51cf9f
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2017-01-30 16:00:44 +1300

    Make team id a argument of ios-deploy-todomvc.sh

--- ios-deploy-todomvc.sh
+++ ios-deploy-todomvc.sh
@@ -14,11 +14,25 @@ function cleanup {
 
 trap cleanup EXIT
 
+if [ -z "$1" ]; then
+  echo "Usage: ./ios-deploy-todomvc.sh [TEAM_ID]" >&2
+  exit 1
+fi
+
 tmpdir=$(mktemp -d)
+# Find the signer given the ou
+signer=`security find-certificate -c "iPhone Developer" -a | grep '^    "alis"<blob>="' | sed 's|    "alis"<blob>="\(.*\)"$|\1|' | while read c; do security find-certificate -c "$c" -p | openssl x509 -subject -noout; done | grep "$1" | sed 's|subject= /UID=[^/]*/CN=\([^/]*\).*|\1|' | head -n 1`
+
+if [ -z "$signer" ]; then
+  echo "Error: No iPhone Developer certificate found for team id $1" >&2
+  exit 1
+fi
 
 nix-build -A ghcIosArm64.reflex-todomvc
 mkdir -p $tmpdir/reflex-todomvc.app
 cp -r `nix-build -A ghcIosArm64.reflex-todomvc`/reflex-todomvc.app/* $tmpdir/reflex-todomvc.app
-/usr/bin/codesign --force --sign "iPhone Developer: Hamish Mackenzie" --entitlements reflex-todomvc/reflex-todomvc.app.xcent --timestamp=none $tmpdir/reflex-todomvc.app
-ios-deploy -b $tmpdir/reflex-todomvc.app "$@"
+sed "s|<team-id/>|$1|" < reflex-todomvc/reflex-todomvc.app.xcent > $tmpdir/reflex-todomvc.app.xcent
+cat $tmpdir/reflex-todomvc.app.xcent
+/usr/bin/codesign --force --sign "$signer" --entitlements $tmpdir/reflex-todomvc.app.xcent --timestamp=none $tmpdir/reflex-todomvc.app
+ios-deploy -b $tmpdir/reflex-todomvc.app
 
