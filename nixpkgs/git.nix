24521bcefd8f67b4c9084f47d01bdb9920a3c496
Ryan Trinkle <ryan.trinkle@gmail.com>
2017-05-01 16:17:33 -0400

    Fix nixpkgs hash

--- nixpkgs/git.nix
+++ nixpkgs/git.nix
@@ -1,5 +1,5 @@
 {
   url = git://github.com/NixOS/nixpkgs;
   rev = "011c149ed5e5a336c3039f0b9d4303020cff1d86";
-  sha256 = "c8de821d548443779bc9cfe1df4d3a5c76e010b3b1b59c08a6779be1d25031ee";
+  sha256 = "03nnlab796bbwvdb9yvb5wqapp2s50q9m69v4kwpf77yvhj1r86y";
 }
