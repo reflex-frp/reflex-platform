1670c5b899658babeda58329d3df6b943cf6aeca
Ryan Trinkle <ryan.trinkle@gmail.com>
2017-08-31 13:41:27 -0400

    Make jsaddle work with original text package again

--- default.nix
+++ default.nix
@@ -355,6 +355,13 @@ let overrideCabal = pkg: f: if pkg == null then null else lib.overrideCabal pkg 
           ${if useReflexOptimizer then "ghcLibdir" else null} = "${ghc.ghcWithPackages (p: [ p.reflex ])}/lib/${ghc.ghc.name}";
         };
 
+        ghcjs-base = doJailbreak (self.callCabal2nix "ghcjs-base" (fetchFromGitHub {
+          owner = "ghcjs";
+          repo = "ghcjs-base";
+          rev = "1f28a93dad380a471a3fd7e00819a88c20fa7f92";
+          sha256 = "1l1mjpb5a0ryamqapm0lirxp58f7w3hk73b18ks62bc9d342zmxm";
+        }) {});
+
         ghc = super.ghc // {
           withPackages = self.ghcWithPackages;
         };
