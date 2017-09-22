b730974b6d6d292f6c0e9f568104a71113bf63ac
Cale Gibbard <cgibbard@gmail.com>
2017-09-22 16:50:24 -0400

    Further shrinking of CrashTest

--- CrashTest.hs
+++ CrashTest.hs
@@ -4,7 +4,6 @@
 
 import Reflex.Dom
 
-import Control.Exception (evaluate)
 import Control.Monad.Trans
 import Data.Time.Clock
 
@@ -13,40 +12,28 @@ import qualified GHCJS.DOM.HTMLTextAreaElement as TA
 import qualified GHCJS.DOM.HTMLElement as El
 import GHCJS.DOM.Types (MonadJSM)
 
+import qualified Data.Text as T
+
 import Emoji
 
 say :: MonadIO m => String -> m ()
 say = liftIO . putStrLn
 
-attachSelectionStart :: (HasJS x (WidgetHost m), PerformEvent t m, MonadJSM (Performable m)) => HTMLTextAreaElement -> Event t a -> m (Event t (Int, a))
-attachSelectionStart t e = performEvent . ffor e $ \v -> do
-  say "a start"
-  n <- TA.getSelectionStart t
-  say "a done"
-  return (n, v)
-
 setSelectionPos :: (HasJS x (WidgetHost m), PerformEvent t m, MonadJSM (Performable m)) => HTMLTextAreaElement -> Event t Int -> m ()
 setSelectionPos t e = performEvent_ . ffor e $ \n -> do 
-  say "s force t"
-  liftIO (evaluate t)
-  say "s force n"
-  liftIO (evaluate n)
-  say "s start"
   TA.setSelectionStart t n
-  say "s end"
   TA.setSelectionEnd t n
-  say "s focus"
   El.focus t
-  say "s done"
 
+main :: IO ()
 main = do
   now <- getCurrentTime
   mainWidget $ do
-    rec tick <- tickLossy 0.05 now
-        let emojiPickE = "blush" <$ tick
+    rec tick <- tickLossy 0.000001 now
+        let emojiPickE = () <$ tick
         t <- textAreaElement $ def { _textAreaElementConfig_setValue = Just (fmap snd newEmoji) }
         setSelectionPos (_textAreaElement_raw t) resetPosE 
-        posPickE <- attachSelectionStart (_textAreaElement_raw t) emojiPickE
-        let newEmoji = attachWith (\s (n, e) -> insertEmojiAt EMPlusOne e n s) (current $ value t) posPickE
+        let newEmoji = attachWith (\s _ -> let (n',s') = insertEmojiAt (T.unpack s) in (n', T.pack s')) 
+                                  (current $ value t) emojiPickE
             resetPosE = fmap fst newEmoji
     return ()
