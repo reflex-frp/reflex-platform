f6b45151226825e2fbe6cff7f303de972ee69780
Luigy Leon <luigy@outlook.com>
2017-10-21 19:12:28 -0400

    Bump ghcjs-base

--- haskell-overlays/text-jsstring/default.nix
+++ haskell-overlays/text-jsstring/default.nix
@@ -25,8 +25,8 @@ self: super: {
     src = fetchFromGitHub {
       owner = "luigy";
       repo = "ghcjs-base";
-      rev = "8569f5d541aa846f2130ff789d19bcd55ea41d2a";
-      sha256 = "1b1fyqgn7jxh4rawgxidacafg6jwfdfcidyh93z6a6lhmm5qaq3n";
+      rev = "e287c5752064a2d3b2c4776a1520e4b0189881b0";
+      sha256 = "01k7wj60gmmf9larjm3gqbsyxwb5xhqr4dyz4xswy78ql845qljd";
     };
     libraryHaskellDepends = with self; [
       base bytestring containers deepseq dlist ghc-prim
