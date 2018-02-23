a626b1a35a540516603c4d37920d443ef508839b
chessai <chessai1996@gmail.com>
2018-02-23 17:40:50 -0500

    fix hashes to use unpacked for nix 2.0

--- nixpkgs/default.nix
+++ nixpkgs/default.nix
@@ -1,1 +1,1 @@
-import ((import ./fetchNixpkgs.nix) { inherit (builtins.fromJSON (builtins.readFile ./github.json)) rev sha256; })
+import ((import ./fetchNixpkgs.nix) { inherit (builtins.fromJSON (builtins.readFile ./github.json)) rev sha256 sha256unpacked; })
