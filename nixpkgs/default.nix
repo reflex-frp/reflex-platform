61ab669d1129619a4ccc7f8ad96048eba7a8cec8
dbornside <dbornside@nixos.office>
2018-03-23 00:19:22 +0000

    use smtp-enabled syslog-ng

--- nixpkgs/default.nix
+++ nixpkgs/default.nix
@@ -1,1 +1,7 @@
-import ((import <nixpkgs> {}).fetchFromGitHub (builtins.fromJSON (builtins.readFile ./github.json)))
+# DO NOT HAND-EDIT THIS FILE
+import ((import <nixpkgs> {}).fetchFromGitHub (
+  let json = builtins.fromJSON (builtins.readFile ./github.json);
+  in { inherit (json) owner repo rev sha256;
+       private = json.private or false;
+     }
+))
