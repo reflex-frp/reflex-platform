003c3efb20da790e111f4ed6184bef21904a8d42
Will Fancher <elvishjerricco@gmail.com>
2018-04-14 08:47:43 -0400

    Bump nix installer to 2.0

--- scripts/installNix.sh
+++ scripts/installNix.sh
@@ -23,22 +23,22 @@ require_util() {
 }
 
 case "$(uname -s).$(uname -m)" in
-    Linux.x86_64) system=x86_64-linux; hash=d7e3cba2103c051b17d2a49dc6389f96ebe46dca72dac8473cfb87af8b67ef03;;
-    Linux.i?86) system=i686-linux; hash=c9f5c41bea715b72eeab83dd70d5cb42c84469a4b419f918df39ebb8597ffec1;;
-    Linux.aarch64) system=aarch64-linux; hash=6b60342fb430a2842b300a72a9cf179dace569713fb27520513c9fcdf94ba0c4;;
-    Darwin.x86_64) system=x86_64-darwin; hash=b3e1a7316053fda14b4c69142c9f432aab2742e3a5febaed805a316450d6360d;;
+    Linux.x86_64) system=x86_64-linux; hash=6312837aee33306cdbb351b75ba1638b89d21b30f0caf0346f9a742425f197ee;;
+    Linux.i?86) system=i686-linux; hash=5333384cf1b2f0d9b66d3c4db2643649f0c20d1ed5a0f0dac47ad0092a5874a3;;
+    Linux.aarch64) system=aarch64-linux; hash=ffe6c5c014c47ca8476c696a55bbd96beffb71f32c5d1a5da66d338a2aebbf17;;
+    Darwin.x86_64) system=x86_64-darwin; hash=6a1f0cabbab18acc8f864b8847e4a44fbb0fadabbbf8940149903af635652be8;;
     *) oops "sorry, there is no binary distribution of Nix for your platform";;
 esac
 
-url="https://nixos.org/releases/nix/nix-1.11.16/nix-1.11.16-$system.tar.bz2"
+url="https://nixos.org/releases/nix/nix-2.0/nix-2.0-$system.tar.bz2"
 
-tarball="$tmpDir/$(basename "$tmpDir/nix-1.11.16-$system.tar.bz2")"
+tarball="$tmpDir/$(basename "$tmpDir/nix-2.0-$system.tar.bz2")"
 
 require_util curl "download the binary tarball"
 require_util bzcat "decompress the binary tarball"
 require_util tar "unpack the binary tarball"
 
-echo "downloading Nix 1.11.16 binary tarball for $system from '$url' to '$tmpDir'..."
+echo "downloading Nix 2.0 binary tarball for $system from '$url' to '$tmpDir'..."
 curl -L "$url" -o "$tarball" || oops "failed to download '$url'"
 
 if type sha256sum > /dev/null 2>&1; then
