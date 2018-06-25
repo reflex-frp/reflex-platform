78541d66819b776a798e26f24bee4547b03a926f
Steffen Eichenberg <Steffen.Eichenberg@lindenbaum.eu>
2018-06-25 08:24:41 +0200

    add additional imports and documentation to project how-to to work around nix bug https://github.com/NixOS/nixpkgs/issues/33592

--- project/default.nix
+++ project/default.nix
@@ -25,8 +25,12 @@ in
 #       };
 #
 #       shells = {
-#         ghc = ["common" "backend" "frontend"];
-#         ghcjs = ["common" "frontend"];
+#         -- because of a bug in nix, we have to add a non base module
+#         -- like "text" to the list of dependencies. This will change with
+#         -- future versions
+#         -- (https://github.com/NixOS/nixpkgs/issues/33592)
+#         ghc = ["common" "backend" "frontend" "text"];
+#         ghcjs = ["common" "frontend" "text"];
 #       };
 #
 #       android.frontend = {
