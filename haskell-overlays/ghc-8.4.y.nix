d27e46c1c493f108df52bfd593e84a38a9395393
Matthew Bauer <mjbauer95@gmail.com>
2018-06-28 17:04:15 -0400

    Bump

--- haskell-overlays/ghc-8.4.y.nix
+++ haskell-overlays/ghc-8.4.y.nix
@@ -6,6 +6,7 @@ self: super: {
   haddock-library-ghcjs = dontCheck super.haddock-library-ghcjs;
   haddock-api-ghcjs = dontCheck super.haddock-api-ghcjs;
 
+  # Versions that haven’t made it into Nixpkgs master yet
   testing-feat = self.callCabal2nix "testing-feat" (fetchFromGitHub {
     owner = "JonasDuregard";
     repo = "testing-feat";
@@ -17,5 +18,11 @@ self: super: {
     repo = "snap-server";
     rev = "b2a888230e107046404b047ff3b8690a592f124c";
     sha256 = "05zbc4lyyphsrkj5h043rgx9gjsgmcd8zahzjz69npd9cf91aa6w";
+  }) {};
+  map-syntax = self.callCabal2nix "map-syntax" (fetchFromGitHub {
+    owner = "mightybyte";
+    repo = "map-syntax";
+    rev = "acebcf0a83ee639e1a0c49850b9c85821d53f621";
+    sha256 = "076knpvls1489gish9z30lhb21vqx44k366vc2i3kdql815v1vqv";
   }) {};
 }
