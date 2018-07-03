b509c34fd351618fbef180af7301cdaa8ceec1fc
Matthew Bauer <mjbauer95@gmail.com>
2018-07-03 18:17:05 -0400

    Override cabal2nix to get newer version

--- default.nix
+++ default.nix
@@ -10,7 +10,25 @@
 , iosSdkVersion ? "10.2"
 }:
 let iosSupport = system != "x86_64-darwin";
-    globalOverlay = self: super: { };
+    globalOverlay = self: super: {
+
+      # need to override cabal2nix to avoid evaluation errors on Android.
+      # https://github.com/NixOS/cabal2nix/pull/344
+      haskellPackages = super.haskellPackages.override {
+        overrides = self_: super_: {
+          cabal2nix = super_.cabal2nix.overrideAttrs (drv: {
+            src = super.fetchFromGitHub {
+              owner = "NixOS";
+              repo = "cabal2nix";
+              rev = "7c77e7e78a94a72681d9d59e69391feada1085fa";
+              sha256 = "1q2bh0xx3srhh52vkahf9mvplzxnb34k7l75qgivh8izcmghaz54";
+            };
+            doCheck = false;
+          });
+        };
+      };
+
+    };
     appleLibiconvHack = self: super: {
       darwin = super.darwin // {
         libiconv =
