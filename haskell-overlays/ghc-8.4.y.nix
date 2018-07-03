d3379a48ff86b227f9ecbfa3b719cb92adba53a4
Matthew Bauer <mjbauer95@gmail.com>
2018-07-03 14:06:45 -0400

    Correct 8.4 overrides
    Most of these *are* in Nixpkgs - they are just not the default ones.

--- haskell-overlays/ghc-8.4.y.nix
+++ haskell-overlays/ghc-8.4.y.nix
@@ -6,43 +6,14 @@ self: super: {
   haddock-library-ghcjs = dontCheck super.haddock-library-ghcjs;
   haddock-api-ghcjs = dontCheck super.haddock-api-ghcjs;
 
-  # Versions that haven’t made it into Nixpkgs master yet
-  testing-feat = self.callCabal2nix "testing-feat" (fetchFromGitHub {
-    owner = "JonasDuregard";
-    repo = "testing-feat";
-    rev = "a1b32842a8cc8ab467d93a9f97f2365330a02113";
-    sha256 = "1pxafc7rdd25j0m0dxgxqhw3vnxb3lw9gv6n885x2lag9mcdkpax";
-  }) {};
-  snap = self.callCabal2nix "snap" (fetchFromGitHub {
-    owner = "snapframework";
-    repo = "snap";
-    rev = "2fa933b52d7d126b59d89eddeed0e8a9d58d1d61";
-    sha256 = "002byv0iqmxj60c1q8ybnipvaqsjy7j7hv8rd7drdbc2cz422wlh";
-  }) {};
-  snap-server = self.callCabal2nix "snap-server" (fetchFromGitHub {
-    owner = "snapframework";
-    repo = "snap-server";
-    rev = "b2a888230e107046404b047ff3b8690a592f124c";
-    sha256 = "05zbc4lyyphsrkj5h043rgx9gjsgmcd8zahzjz69npd9cf91aa6w";
-  }) {};
-  heist = self.callCabal2nix "heist" (fetchFromGitHub {
-    owner = "snapframework";
-    repo = "heist";
-    rev = "3ccbec548830abce7ed7eba42c1c294b02b6cd52";
-    sha256 = "14sd4d4an7fj8yb4mr8cdallsv69x5jb1hd330sg10ahi1ryzspr";
-  }) {};
-  map-syntax = self.callCabal2nix "map-syntax" (fetchFromGitHub {
-    owner = "mightybyte";
-    repo = "map-syntax";
-    rev = "acebcf0a83ee639e1a0c49850b9c85821d53f621";
-    sha256 = "076knpvls1489gish9z30lhb21vqx44k366vc2i3kdql815v1vqv";
-  }) {};
-  logging-effect = self.callCabal2nix "logging-effect" (fetchFromGitHub {
-    owner = "ocharles";
-    repo = "logging-effect";
-    rev = "bd65c0d0d54f096ab90810b8c3604dd9c110a4ab";
-    sha256 = "1zlh8h4gq4kb8j9cgxsaas3h0havq23rrg0pl63d2ii1vghi9181";
-  }) {};
+  # Versions that haven’t made it into the Haskell LTS release yet
+  testing-feat = super.testing-feat_1_1_0_0;
+  snap = super.snap_1_1_1_0;
+  snap-server = super.snap-server_1_1_0_0;
+  heist = dontCheck super.heist_1_1;
+  map-syntax = super.map-syntax_0_3;
+  logging-effect = super.logging-effect_1_3_1;
+
   algebraic-graphs = doJailbreak super.algebraic-graphs;
 
   # Broken in master
