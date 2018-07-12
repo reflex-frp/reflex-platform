5a0836d9d7e49adad2ff5b720554458e60e5db5b
Matthew Bauer <mjbauer95@gmail.com>
2018-07-12 13:35:03 -0400

    Update patch

--- haskell-overlays/save-splices.nix
+++ haskell-overlays/save-splices.nix
@@ -3,12 +3,6 @@
 self: super: {
   mkDerivation = attrs: let
     drv = (super.mkDerivation (attrs // {
-      # ANN is unimplemented in splices.patch
-      postPatch = ''
-        ${attrs.postPatch or ""}
-        find . -name '*.hs' -exec sed -i 's/^{-# ANN .* #-}$//' '{}' \;
-      '';
-
       preConfigure = ''
         ${attrs.preConfigure or ""}
         configureFlags+=" --ghc-option=-save-splices=$out$SPLICE_DIR"
