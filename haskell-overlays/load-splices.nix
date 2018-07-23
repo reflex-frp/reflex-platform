b6a9066eb1e2102abf5df7ad7a94e2e713bcd6be
Matthew Bauer <matthew.bauer@obsidian.systems>
2018-07-23 16:38:39 -0400

    Fix attrName

--- haskell-overlays/load-splices.nix
+++ haskell-overlays/load-splices.nix
@@ -5,7 +5,8 @@ self: super: {
   # Add some flags to load splices from nativeHaskellPackages
   mkDerivation = drv: super.mkDerivation (drv // {
     configureFlags = let
-      attrName = "${drv.pname}_${lib.replaceStrings ["."] ["_"] drv.version}";
+      attrName = drv.pname;
+      # attrName = "${drv.pname}_${lib.replaceStrings ["."] ["_"] drv.version}";
       pkg = builtins.getAttr attrName nativeHaskellPackages;
     in (drv.configureFlags or []) ++
       (lib.optionals (builtins.hasAttr attrName nativeHaskellPackages) [
