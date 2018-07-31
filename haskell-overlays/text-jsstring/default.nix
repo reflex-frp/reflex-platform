2cfb768f3fb8295cbbbcea32c8b298364b310c3b
Matthew Bauer <matthew.bauer@obsidian.systems>
2018-07-30 20:12:55 -0400

    ghcjs: turn off full laziness, rewrite rules
    again these are probably important to have, but having them on
    disables the ghcjs inlining. This is less than the old ones, but
    probably still not enough optimizations for ghcjs to work properly.

--- haskell-overlays/text-jsstring/default.nix
+++ haskell-overlays/text-jsstring/default.nix
@@ -10,6 +10,12 @@ self: super: {
     rev = "78f714da9bd3510e348b6341855be925b8ede949";
     sha256 = "003lggllk1hjzjx8wfl941j2bn13sgpxz9bqgvsn6bhlrhwnwcsl";
   }) {};
+  mkDerivation = attrs: (super.mkDerivation attrs // {
+    configureFlags = (drv.configureFlags or []) ++ [
+      "--ghcjs-option=-fno-full-laziness"
+      "--ghcjs-option=-fno-enable-rewrite-rules"
+    ];
+  });
   jsaddle = overrideCabal super.jsaddle (drv: {
     buildDepends = (drv.buildDepends or []) ++ [
       self.ghcjs-base
