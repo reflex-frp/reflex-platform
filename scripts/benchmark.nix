e1b785d4c4f071701022a53a2aaf0ef65b327f25
Luigy Leon <Luigy.Leon@Obsidian.Systems>
2018-09-07 07:38:10 -0400

    Pin benchmark's node deps
    Fixes running the benchmarks and switches to using yarn/yarn2nix.

--- scripts/benchmark.nix
+++ scripts/benchmark.nix
@@ -1,5 +1,39 @@
 { reflex-platform ? import ./.. {} }:
 let pkgs = reflex-platform.nixpkgs;
+    shellHook = pkgs.yarn2nix.linkNodeModulesHook + ''
+      export PATH=node_modules/.bin:$PATH
+    '';
+    inherit (pkgs) mkYarnPackage;
+    inherit (reflex-platform) js-framework-benchmark-src;
+    nodePkgs = {
+      webdriver-ts = mkYarnPackage {
+        name = "webdriver-ts";
+        src = js-framework-benchmark-src + /webdriver-ts;
+        preInstall = "npm run build-prod";
+        inherit shellHook;
+        yarnLock = ./webdriver-ts.yarn.lock;
+      };
+      webdriver-ts-results = mkYarnPackage {
+        name = "webdriver-ts-results";
+        src = js-framework-benchmark-src + /webdriver-ts-results;
+        preInstall = "npm run build-prod";
+        inherit shellHook;
+        yarnLock = ./webdriver-ts-results.yarn.lock;
+      };
+      vanillajs-keyed = mkYarnPackage {
+        name = "vanillajs-keyed";
+        src = js-framework-benchmark-src + /vanillajs-keyed;
+        preInstall = "npm run build-prod";
+        inherit shellHook;
+        yarnLock = ./vanillajs-keyed.yarn.lock;
+      };
+      js-framework-benchmark = mkYarnPackage {
+        name = "js-framework-benchmark";
+        src = js-framework-benchmark-src;
+        yarnLock = ./js-framework-benchmark.yarn.lock;
+        inherit shellHook;
+      };
+    };
 in pkgs.writeScript "benchmark.sh" ''
 #!/usr/bin/env bash
 set -euo pipefail
@@ -7,7 +41,7 @@ set -euo pipefail
 exec 3>&1
 exec 1>&2
 
-PATH="${pkgs.nodejs-8_x}/bin:${pkgs.nodePackages.npm}/bin:${pkgs.chromedriver}/bin:$PATH"
+PATH="${pkgs.yarn}/bin:${pkgs.nodejs-8_x}/bin:${pkgs.nodePackages.npm}/bin:${pkgs.chromedriver}/bin:$PATH"
 CHROME_BINARY="${if reflex-platform.system == "x86_64-darwin"
   then ""
   else ''--chromeBinary "${pkgs.chromium}/bin/chromium"''
@@ -25,19 +59,16 @@ cd "$CLEAN"
 cp -a "${reflex-platform.js-framework-benchmark-src}/"* .
 chmod -R +w .
 
-npm install
-for package in webdriver-ts webdriver-ts-results vanillajs-keyed; do
-    cd $package
-    npm install
-    npm run build-prod
-    cd ..
-done
+ln -s ${nodePkgs.js-framework-benchmark.node_modules} .
+rm -r yarn.lock vanillajs-keyed webdriver-ts-results
+ln -s ${nodePkgs.vanillajs-keyed}/node_modules/vanillajs-keyed .
+ln -s ${nodePkgs.webdriver-ts-results}/node_modules/webdriver-ts-results .
 
 REFLEX_DOM_DIST=reflex-dom-v0.4-keyed/dist
 mkdir -p "$REFLEX_DOM_DIST"
 cp -a "${reflex-platform.ghcjs.reflex-dom}/bin/krausest.jsexe/"* "$REFLEX_DOM_DIST"
 
-npm start > server.out &
+yarn run start > server.out &
 SERVER_PID=$!
 
 # ensures that grep will block execution but tail won't
@@ -45,8 +76,9 @@ SERVER_PID=$!
 SERVER_PORT="$((tail -f -n0 server.out & ) | grep -m 1 '127.0.0.1' | sed -e 's/.*127.0.0.1://')"
 
 cd webdriver-ts
+ln -s "${nodePkgs.webdriver-ts}/node_modules/webdriver-ts/dist" .
 
-npm run selenium -- --framework vanillajs-keyed reflex --count 1 --headless $CHROME_BINARY $CHROMEDRIVER --port $SERVER_PORT
+yarn run selenium --framework vanillajs-keyed reflex --count 1 --headless $CHROME_BINARY $CHROMEDRIVER --port $SERVER_PORT
 
 kill "$SERVER_PID"
 
