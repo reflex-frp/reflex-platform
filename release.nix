7d27cc5ab110644dead521de5f62eb5b31040602
John Ericson <John.Ericson@Obsidian.Systems>
2018-09-19 15:58:59 -0400

    reflexPlatform -> reflex-platform

--- release.nix
+++ release.nix
@@ -1,28 +1,28 @@
 {}:
 with import ./. {};
 let inherit (nixpkgs.lib) optionals;
-    getOtherDeps = reflexPlatform: [
-      reflexPlatform.stage2Script
-      reflexPlatform.nixpkgs.cabal2nix
-      reflexPlatform.ghc.cabal2nix
+    getOtherDeps = reflex-platform: [
+      reflex-platform.stage2Script
+      reflex-platform.nixpkgs.cabal2nix
+      reflex-platform.ghc.cabal2nix
     ] ++ builtins.concatLists (map
       (crossPkgs: optionals (crossPkgs != null) [
         crossPkgs.buildPackages.haskellPackages.cabal2nix
       ]) [
-        reflexPlatform.nixpkgsCross.ios.aarch64
-        reflexPlatform.nixpkgsCross.android.aarch64
-        reflexPlatform.nixpkgsCross.android.aarch32
+        reflex-platform.nixpkgsCross.ios.aarch64
+        reflex-platform.nixpkgsCross.android.aarch64
+        reflex-platform.nixpkgsCross.android.aarch32
       ]
     );
 
 in nixpkgs.lib.genAttrs cacheBuildSystems (system:
   let
-    reflexPlatform = (import ./. { inherit system; iosSupportForce = true; });
+    reflex-platform = (import ./. { inherit system; iosSupportForce = true; });
   in {
-    tryReflexShell = reflexPlatform.tryReflexShell;
-    skeleton-test = import ./skeleton-test.nix { this = reflexPlatform; };
+    tryReflexShell = reflex-platform.tryReflexShell;
+    skeleton-test = import ./skeleton-test.nix { this = reflex-platform; };
   } // nixpkgs.lib.listToAttrs
-    (builtins.map (drv: { inherit (drv) name; value = drv; }) (getOtherDeps reflexPlatform))
+    (builtins.map (drv: { inherit (drv) name; value = drv; }) (getOtherDeps reflex-platform))
 ) // {
   benchmark = import ./scripts/benchmark.nix {};
   inherit sources iosReflexTodomvc androidReflexTodomvc;
