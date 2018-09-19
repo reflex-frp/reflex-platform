c4151f36f5a43d6e794ae814c740fee993e9e544
John Ericson <John.Ericson@Obsidian.Systems>
2018-09-19 16:07:55 -0400

    release.nix: Finish sorting jobs by build platform

--- release.nix
+++ release.nix
@@ -1,6 +1,5 @@
-{}:
-with import ./. {};
-let inherit (nixpkgs) lib;
+let local-reflex-platform = import ./. {};
+    inherit (local-reflex-platform.nixpkgs) lib;
     getOtherDeps = reflex-platform: [
       reflex-platform.stage2Script
       reflex-platform.nixpkgs.cabal2nix
@@ -15,15 +14,17 @@ let inherit (nixpkgs) lib;
       ]
     );
 
-in lib.genAttrs cacheBuildSystems (system:
+in lib.genAttrs local-reflex-platform.cacheBuildSystems (system:
   let
-    reflex-platform = (import ./. { inherit system; iosSupportForce = true; });
+    reflex-platform = (import ./. { inherit system; iosSupportForce = system == "x86_64-darwin"; });
   in {
     tryReflexShell = reflex-platform.tryReflexShell;
     skeleton-test = import ./skeleton-test.nix { inherit reflex-platform; };
+    benchmark = import ./scripts/benchmark.nix { inherit reflex-platform; };
+  } // lib.optionalAttrs (reflex-platform.androidSupport) {
+    inherit (reflex-platform) androidReflexTodomvc;
+  } // lib.optionalAttrs (reflex-platform.iosSupport) {
+    inherit (reflex-platform) iosReflexTodomvc;
   } // lib.listToAttrs
     (builtins.map (drv: { inherit (drv) name; value = drv; }) (getOtherDeps reflex-platform))
-) // {
-  benchmark = import ./scripts/benchmark.nix {};
-  inherit sources iosReflexTodomvc androidReflexTodomvc;
-}
+  )
