4bd107e5d68493529fb27a2e0bf6af24d1ce1268
John Ericson <Ericson2314@Yahoo.com>
2018-09-20 22:45:20 -0400

    More jobs for older GHCs

--- release.nix
+++ release.nix
@@ -29,7 +29,10 @@ let local-reflex-platform = import ./. {};
             echo $out/bin/reflex-todomvc.jsexe >> $out/nix-support/hydra-build-products
           '';
         });
-        ghcReflexTodomvc = reflex-platform.ghc.reflex-todomvc;
+        ghc.ReflexTodomvc = reflex-platform.ghc.reflex-todomvc;
+        ghc8_0.reflexTodomvc = reflex-platform.ghc8_0.reflex-todomvc;
+        ghc8_2.reflexTodomvc = reflex-platform.ghc8_2.reflex-todomvc;
+        ghc8_4.reflexTodomvc = reflex-platform.ghc8_4.reflex-todomvc;
         skeleton-test = import ./skeleton-test.nix { inherit reflex-platform; };
         benchmark = import ./scripts/benchmark.nix { inherit reflex-platform; };
         cache = reflex-platform.pinBuildInputs
