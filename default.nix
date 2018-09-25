55df843bb96cc60e8708a8b183583ec97e401d86
John Ericson <John.Ericson@Obsidian.Systems>
2018-09-25 02:30:28 -0400

    No more `with lib; with haskellLib;`

--- default.nix
+++ default.nix
@@ -210,13 +210,9 @@ let iosSupport =
       ghcjs = hackGet ./ghcjs;
     };
 
-    inherit (nixpkgs.stdenv.lib) optional optionals optionalAttrs;
-
     optionalExtension = cond: overlay: if cond then overlay else _: _: {};
 
-in with lib; with haskellLib;
-
-let overrideCabal = pkg: f: if pkg == null then null else haskellLib.overrideCabal pkg f;
+    overrideCabal = pkg: f: if pkg == null then null else haskellLib.overrideCabal pkg f;
 
     replaceSrc = pkg: src: version: overrideCabal pkg (drv: {
       inherit src version;
@@ -225,7 +221,7 @@ let overrideCabal = pkg: f: if pkg == null then null else haskellLib.overrideCab
       editedCabalFile = null;
     });
 
-    combineOverrides = old: new: old // new // optionalAttrs (old ? overrides && new ? overrides) {
+    combineOverrides = old: new: old // new // lib.optionalAttrs (old ? overrides && new ? overrides) {
       overrides = lib.composeExtensions old.overrides new.overrides;
     };
 
@@ -582,12 +578,12 @@ in let this = rec {
       nodejs
       pkgconfig
       closurecompiler;
-  } // (optionalAttrs (!(haskellPackages.ghc.isGhcjs or false) && builtins.compareVersions haskellPackages.ghc.version "8.2" < 0) {
+  } // (lib.optionalAttrs (!(haskellPackages.ghc.isGhcjs or false) && builtins.compareVersions haskellPackages.ghc.version "8.2" < 0) {
     # ghc-mod doesn't currently work on ghc 8.2.2; revisit when https://github.com/DanielG/ghc-mod/pull/911 is closed
     # When ghc-mod is included in the environment without being wrapped in justStaticExecutables, it prevents ghc-pkg from seeing the libraries we install
     ghc-mod = (nixpkgs.haskell.lib.justStaticExecutables nativeHaskellPackages.ghc-mod);
     inherit (haskellPackages) hdevtools;
-  }) // (optionalAttrs (builtins.compareVersions haskellPackages.ghc.version "7.10" >= 0) {
+  }) // (lib.optionalAttrs (builtins.compareVersions haskellPackages.ghc.version "7.10" >= 0) {
     inherit (nativeHaskellPackages) stylish-haskell; # Recent stylish-haskell only builds with AMP in place
   });
 
@@ -633,7 +629,7 @@ in let this = rec {
           "testSystemDepends"
           "testToolDepends"
         ];
-        concatCombinableAttrs = haskellConfigs: filterAttrs (n: v: v != []) (listToAttrs (map (name: { inherit name; value = concatLists (map (haskellConfig: haskellConfig.${name} or []) haskellConfigs); }) combinableAttrs));
+        concatCombinableAttrs = haskellConfigs: lib.filterAttrs (n: v: v != []) (lib.listToAttrs (map (name: { inherit name; value = concatLists (map (haskellConfig: haskellConfig.${name} or []) haskellConfigs); }) combinableAttrs));
         getHaskellConfig = p: (overrideCabal p (args: {
           passthru = (args.passthru or {}) // {
             out = args;
@@ -642,8 +638,8 @@ in let this = rec {
         notInTargetPackageSet = p: all (pname: (p.pname or "") != pname) packageNames;
         baseTools = generalDevToolsAttrs env;
         overriddenTools = attrValues (baseTools // shellToolOverrides env baseTools);
-        depAttrs = mapAttrs (_: v: filter notInTargetPackageSet v) (concatCombinableAttrs (concatLists [
-          (map getHaskellConfig (attrVals packageNames env))
+        depAttrs = lib.mapAttrs (_: v: filter notInTargetPackageSet v) (concatCombinableAttrs (concatLists [
+          (map getHaskellConfig (lib.attrVals packageNames env))
           [{
             buildTools = overriddenTools ++ tools env;
           }]
@@ -687,16 +683,16 @@ in let this = rec {
     ++ builtins.map reflexEnv platforms;
 
   cachePackages =
-    let otherPlatforms = optionals androidSupport [
+    let otherPlatforms = lib.optionals androidSupport [
           "ghcAndroidAarch64"
           "ghcAndroidAarch32"
-        ] ++ optional iosSupport "ghcIosAarch64";
+        ] ++ lib.optional iosSupport "ghcIosAarch64";
     in tryReflexPackages
       ++ builtins.map reflexEnv otherPlatforms
-      ++ optionals androidSupport [
+      ++ lib.optionals androidSupport [
         androidDevTools
         androidReflexTodomvc
-      ] ++ optionals iosSupport [
+      ] ++ lib.optionals iosSupport [
         iosReflexTodomvc
       ];
 
