b31edab2af0e6172af581892109c15083e0e07ad
John Ericson <Ericson2314@Yahoo.com>
2018-10-01 20:10:37 +0000

    GHC 8.2: enable-fPIC patch no longer needed

--- nixpkgs-overlays/mobile-ghc/default.nix
+++ nixpkgs-overlays/mobile-ghc/default.nix
@@ -11,8 +11,6 @@ self: super: {
           ./8.2.y/android-patches/undefine_MYTASK_USE_TLV_for_CC_SUPPORTS_TLS_zero.patch
           ./8.2.y/android-patches/force-relocation-equal-pic.patch
           ./8.2.y/android-patches/rts_android_log_write.patch
-        ] ++ lib.optionals (self.stdenv.targetPlatform != self.stdenv.hostPlatform) [
-          ./8.2.y/android-patches/enable-fPIC.patch
         ] ++ lib.optionals (with self.stdenv.targetPlatform; (isDarwin && (isAarch64 || isArm))) [
           ./8.2.y/ios-rump-linker.patch
         ];
