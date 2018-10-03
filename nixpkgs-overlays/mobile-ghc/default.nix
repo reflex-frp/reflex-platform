79c925768b2375b0b242c079f548d1990192b56f
John Ericson <John.Ericson@Obsidian.Systems>
2018-10-03 15:05:24 -0400

    Add monstrous patch for ghc 8.2 and iOS
    Thankfully, most of these changes are cherry-picks from commits that
    made it into 8.4. The little at the end that isn't is a) stuff that
    should be upstreamed for all versions, and b) a quick hack just
    disabling some linker routines and iserv.

--- nixpkgs-overlays/mobile-ghc/default.nix
+++ nixpkgs-overlays/mobile-ghc/default.nix
@@ -11,8 +11,13 @@ self: super: {
           ./8.2.y/android-patches/undefine_MYTASK_USE_TLV_for_CC_SUPPORTS_TLS_zero.patch
           ./8.2.y/android-patches/force-relocation-equal-pic.patch
           ./8.2.y/android-patches/rts_android_log_write.patch
-        ] ++ lib.optionals (with self.stdenv.targetPlatform; (isDarwin && (isAarch64 || isArm))) [
-          ./8.2.y/ios-rump-linker.patch
+        ] ++ lib.optionals self.stdenv.targetPlatform.isiOS [
+          ./8.2.y/fixed-ios.patch
+          #(self.buildPackages.fetchpatch {
+          #  url = "https://patch-diff.githubusercontent.com/raw/obsidiansystems/ghc/pull/1.patch";
+          #  sha256 = "0h34fd55qng3xrnm6x6jy56f7rbmng55afy56r84lvrlk1y8gjwf";
+          #  excludes = [ "testsuite" ];
+          #})
         ];
       });
     } // lib.mapAttrs (n: v: v.overrideAttrs (drv: {
