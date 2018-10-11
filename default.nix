dde05910e069cf1fad20bfa0b205ff265ae5d21a
John Ericson <git@JohnEricson.me>
2018-10-11 15:25:07 -0400

    Make pinBuildInputs make proper hydra aggregate job

--- default.nix
+++ default.nix
@@ -726,13 +726,15 @@ in let this = rec {
   # A simple derivation that just creates a file with the names of all
   # of its inputs. If built, it will have a runtime dependency on all
   # of the given build inputs.
-  pinBuildInputs = drvName: buildInputs: otherDeps: nixpkgs.runCommand drvName {
-    buildCommand = ''
-      mkdir "$out"
+  pinBuildInputs = name: buildInputs: otherDeps: (nixpkgs.releaseTools.aggregate {
+    inherit name;
+    constituents = buildInputs ++ otherDeps;
+  }).overrideAttrs (old: {
+    buildCommand = old.buildCommand + ''
       echo "$propagatedBuildInputs $buildInputs $nativeBuildInputs $propagatedNativeBuildInputs $otherDeps" > "$out/deps"
     '';
     inherit buildInputs otherDeps;
-  } "";
+  });
 
   # The systems that we want to build for on the current system
   cacheTargetSystems = lib.warn "cacheTargetSystems has been deprecated, use cacheBuildSystems" cacheBuildSystems;
