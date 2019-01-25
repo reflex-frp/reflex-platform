328d70e18b698f92bf2f9104c1bf8a8bb3a9834c
Matthew Bauer <mjbauer95@gmail.com>
2019-01-25 16:39:38 -0500

    Update nixpkgs

--- nixpkgs/default.nix
+++ nixpkgs/default.nix
@@ -1,8 +1,21 @@
+# DO NOT HAND-EDIT THIS FILE
 let
-  fetchFromGitHub = if (builtins ? "fetchTarball")
-    then { owner, repo, rev, sha256 }: builtins.fetchTarball {
-        inherit sha256;
-        url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz";
-      }
+  fetch = { private ? false, ... }@args:
+    if private && builtins.hasAttr "fetchGit" builtins
+    then fetchFromGitHubPrivate args
+    else fetchFromGitHub (builtins.removeAttrs args ["branch"]);
+  fetchFromGitHub = if builtins.hasAttr "fetchTarball" builtins then
+    ({ owner, repo, rev, sha256, ... }: builtins.fetchTarball {
+      url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz";
+      inherit sha256; })
     else (import <nixpkgs> {}).fetchFromGitHub;
-in import (fetchFromGitHub (builtins.fromJSON (builtins.readFile ./github.json)))
+  fetchFromGitHubPrivate =
+    { owner, repo, rev, branch ? null, name ? null, sha256 ? null
+    , private ? false, fetchSubmodules ? false
+    , githubBase ? "github.com", ...}: assert !fetchSubmodules;
+      builtins.fetchGit ({
+        url = "ssh://git@${githubBase}/${owner}/${repo}.git";
+        inherit rev;
+      } // (if branch == null then {} else { ref = branch; })
+        // (if name == null then {} else { inherit name; }));
+in import (fetch (builtins.fromJSON (builtins.readFile ./github.json)))
