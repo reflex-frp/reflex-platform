21a61e0074c5695d16c908d1b6d13da48eed9636
Ken Micklas <ken.micklas@obsidian.systems>
2019-03-28 11:22:14 -0400

    Force GHC's GET_ARM_ISA to report armv7 on Android ARM 32-bit

--- nixpkgs-overlays/mobile-ghc/default.nix
+++ nixpkgs-overlays/mobile-ghc/default.nix
@@ -5,7 +5,10 @@ self: super: {
     compiler = super.haskell.compiler // lib.mapAttrs (n: v: v.overrideAttrs (drv: {
       patches = (drv.patches or []) ++ lib.optionals self.stdenv.targetPlatform.useAndroidPrebuilt [
         ./8.4.y/android-patches/force-relocation.patch
+        ./8.4.y/android-patches/force-armv7.patch
       ];
+      # Since force-armv7.patch applies to aclocal.m4, we need to regenerate configure.
+      nativeBuildInputs = (drv.nativeBuildInputs or []) ++ [self.autoreconfHook];
     })) { inherit (super.haskell.compiler) ghc843 ghcHEAD ghcSplices; };
   };
 }
