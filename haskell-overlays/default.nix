daabd633155da624198ed3302551bb2e834d2ac0
Ken Micklas <ken.micklas@obsidian.systems>
2019-03-29 11:29:05 -0400

    Remove support for GHCs older than 8.4

--- haskell-overlays/default.nix
+++ haskell-overlays/default.nix
@@ -43,7 +43,6 @@ rec {
     (optionalExtension (super.ghc.isGhcjs or false) combined-ghcjs)
 
     (optionalExtension (super.ghc.isGhcjs or false && useTextJSString) textJSString)
-    (optionalExtension (with nixpkgs.stdenv; versionWildcard [ 8 2 ] super.ghc.version && hostPlatform != buildPlatform) disableTemplateHaskell)
     (optionalExtension (with nixpkgs.stdenv; versionWildcard [ 8 4 ] super.ghc.version && hostPlatform != buildPlatform) loadSplices)
 
     (optionalExtension (nixpkgs.stdenv.hostPlatform.useAndroidPrebuilt or false) android)
@@ -54,33 +53,22 @@ rec {
 
   combined-any = self: super: foldExtensions [
     any
-    (optionalExtension (versionWildcard [ 7 ] (getGhcVersion super.ghc)) combined-any-7)
     (optionalExtension (versionWildcard [ 8 ] (getGhcVersion super.ghc)) combined-any-8)
-  ] self super;
-
-  combined-any-7 = self: super: foldExtensions [
-    any-7
-    (optionalExtension (versionWildcard [ 7 8 ] (getGhcVersion super.ghc)) any-7_8)
   ] self super;
 
   combined-any-8 = self: super: foldExtensions [
     any-8
-    (optionalExtension (versionWildcard [ 8 0 ] (getGhcVersion super.ghc)) any-8_0)
     (optionalExtension (versionWildcard [ 8 4 ] (getGhcVersion super.ghc)) any-8_4)
     (optionalExtension (lib.versionOlder "8.5"  (getGhcVersion super.ghc)) any-head)
   ] self super;
 
   combined-ghc = self: super: foldExtensions [
-    (optionalExtension (versionWildcard [ 8 0 ] super.ghc.version) ghc-8_0)
-    (optionalExtension (versionWildcard [ 8 2 ] super.ghc.version) ghc-8_2)
     (optionalExtension (versionWildcard [ 8 4 ] super.ghc.version) ghc-8_4)
     (optionalExtension (lib.versionOlder "8.5"  super.ghc.version) ghc-head)
   ] self super;
 
   combined-ghcjs = self: super: foldExtensions [
     ghcjs
-    (optionalExtension (versionWildcard [ 8 0 ] super.ghc.ghcVersion) ghcjs-8_0)
-    (optionalExtension (versionWildcard [ 8 2 ] super.ghc.ghcVersion) ghcjs-8_2)
     (optionalExtension (versionWildcard [ 8 4 ] super.ghc.ghcVersion) ghcjs-8_4)
   ] self super;
 
@@ -102,16 +90,11 @@ rec {
 
   # For GHC and GHCJS
   any = _: _: {};
-  any-7 = import ./any-7.nix { inherit haskellLib; };
-  any-7_8 = import ./any-7.8.nix { inherit haskellLib; };
   any-8 = import ./any-8.nix { inherit haskellLib lib getGhcVersion; };
-  any-8_0 = import ./any-8.0.nix { inherit haskellLib; };
   any-8_4 = import ./any-8.4.nix { inherit haskellLib fetchFromGitHub; inherit (nixpkgs) pkgs; };
   any-head = import ./any-head.nix { inherit haskellLib fetchFromGitHub; };
 
   # Just for GHC, usually to sync with GHCJS
-  ghc-8_0 = import ./ghc-8.0.nix { inherit haskellLib stage2Script; };
-  ghc-8_2 = _: _: {};
   ghc-8_4 = _: _: {};
   ghc-head = _: _: {};
 
@@ -127,13 +110,6 @@ rec {
   # Just for GHCJS
   ghcjs = import ./ghcjs.nix {
     inherit haskellLib nixpkgs fetchFromGitHub ghcjsBaseSrc useReflexOptimizer;
-  };
-  ghcjs-8_0 = self: super: {
-    hashable = haskellLib.addBuildDepend (self.callHackage "hashable" "1.2.7.0" {}) self.text;
-    # `configure` cannot be generated on the fly from `configure.ac` with older Cabal.
-    old-time = haskellLib.addBuildTool super.old-time nixpkgs.autoreconfHook;
-  };
-  ghcjs-8_2 = _: _: {
   };
   ghcjs-8_4 = optionalExtension useTextJSString (_: _: {
     dlist = null;
