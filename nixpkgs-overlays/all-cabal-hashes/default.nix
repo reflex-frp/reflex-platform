f56f8a08dc9d9213261ae1e80f4e8fe2d127c5e6
Ali Abrar <aliabrar@gmail.com>
2019-05-16 16:52:43 -0400

    Update all-cabal-hashes

--- nixpkgs-overlays/all-cabal-hashes/default.nix
+++ nixpkgs-overlays/all-cabal-hashes/default.nix
@@ -1,8 +1,8 @@
 self: _: {
 
   all-cabal-hashes = self.fetchurl {
-    url = "https://github.com/commercialhaskell/all-cabal-hashes/archive/98ec0eee2ddef5d4a00e2ac4a95e8add46d23b69.tar.gz";
-    sha256 = "0svcaaflqi5c815z3yrh61bjny1jnwp42sylmsnwryjldqvizc1a";
+    url = "https://github.com/commercialhaskell/all-cabal-hashes/archive/32e99465c4d09f1b4a5c80fbdca56381b06bf854.tar.gz";
+    sha256 = "023ysdp7iypjqmb90hpq9dabzbxi13ybkrvgpn34l9qpihz24zax";
   };
 
 }
