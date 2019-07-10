0e1c1f790f32f35f3b09891d8dca310c7700b32e
Ali Abrar <aliabrar@gmail.com>
2019-07-10 17:56:52 -0400

    Update force-relocation patch ghc version; Temporarily check in a patch to nixpkgs while we wait to see if it fixes android builds

--- nixpkgs-overlays/mobile-ghc/default.nix
+++ nixpkgs-overlays/mobile-ghc/default.nix
@@ -4,8 +4,8 @@ self: super: {
   haskell = super.haskell // {
     compiler = super.haskell.compiler // lib.mapAttrs (n: v: v.overrideAttrs (drv: {
       patches = (drv.patches or []) ++ lib.optionals self.stdenv.targetPlatform.useAndroidPrebuilt [
-        ./8.4.y/android-patches/force-relocation.patch
+        ./8.6.y/android-patches/force-relocation.patch
       ];
-    })) { inherit (super.haskell.compiler) ghc844 ghcHEAD ghcSplices; };
+    })) { inherit (super.haskell.compiler) ghc865 ghcSplices; };
   };
 }
