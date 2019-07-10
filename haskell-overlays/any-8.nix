086ffd9311a4cb2834a47e5a13a02a7d732655f6
Ali Abrar <aliabrar@gmail.com>
2019-07-10 00:15:17 -0400

    Remove more commented out overrides

--- haskell-overlays/any-8.nix
+++ haskell-overlays/any-8.nix
@@ -1,11 +1,4 @@
 { lib, haskellLib, getGhcVersion }:
 with haskellLib;
 self: super: lib.optionalAttrs (lib.versionOlder (getGhcVersion super.ghc) "8.6.0") {
-  # TODO document why
-  # concurrent-output = haskellLib.doJailbreak super.concurrent-output;
-  # # Newer versions cause some sort of issues with multiple `(<>)` definitions.
-  # # Seems to be that semigroup-monoid stuff is being CPP'd incorrectly.
-  # base-compat= self.callHackage "base-compat" "0.9.3" {};
-  # haddock-library = doJailbreak (self.callHackage "haddock-library" "1.7.0" {});
-  # entropy = self.callHackage "entropy" "0.4.1.4" {};
 }
