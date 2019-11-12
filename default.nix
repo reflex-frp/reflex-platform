54f2b568631e3875a09705f35be2038438bb157e
John Ericson <John.Ericson@Obsidian.Systems>
2019-11-12 23:44:11 +0000

    Fix GHC submodule, and skip patches
    I think those patches have now been applied.

--- default.nix
+++ default.nix
@@ -28,6 +28,7 @@ let iosSupport = system == "x86_64-darwin";
               echo ${drv.version} >VERSION
               ./boot
             '' + drv.preConfigure or "";
+            patches = [];
           });
         };
         packages = super.haskell.packages // {
