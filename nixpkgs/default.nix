10f890dace11f0383776ff4bcfd4867ea121a761
Matthew Bauer <mjbauer95@gmail.com>
2019-11-26 16:43:14 -0500

    Use ob thunkable nixpkgs/default.nix

--- nixpkgs/default.nix
+++ nixpkgs/default.nix
@@ -1,9 +1,7 @@
-let
-  fetchFromGitHub = { owner, repo, rev, sha256, branch }:
-    if (builtins ? "fetchTarball")
-    then builtins.fetchTarball {
-        inherit sha256;
-        url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz";
-      }
-    else (import <nixpkgs> {}).fetchFromGitHub { inherit owner repo rev; };
-in import (fetchFromGitHub (builtins.fromJSON (builtins.readFile ./github.json)))
+# DO NOT HAND-EDIT THIS FILE
+import ((import <nixpkgs> {}).fetchFromGitHub (
+  let json = builtins.fromJSON (builtins.readFile ./github.json);
+  in { inherit (json) owner repo rev sha256;
+       private = json.private or false;
+     }
+))
