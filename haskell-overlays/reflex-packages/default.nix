abe99ce6874969ccf2ed9e96f68f4b29d504a271
John Ericson <git@JohnEricson.me>
2019-12-25 00:06:18 -0500

    Add comment explaining why `--subpath` is needed

--- haskell-overlays/reflex-packages/default.nix
+++ haskell-overlays/reflex-packages/default.nix
@@ -147,6 +147,7 @@ in
   dependent-sum-template = self.callHackage "dependent-sum-template" "0.1.0.0" {};
   dependent-sum-universe-orphans = self.callCabal2nix "dependent-sum-universe-orphans" self._dep.dependent-sum-universe-orphans {};
 
+  # Need to use `--subpath` because LICENSE in each dir is a symlink to the repo root.
   universe = self.callCabal2nixWithOptions "universe" universeRepo "--subpath universe" {};
   universe-base = self.callCabal2nixWithOptions "universe" universeRepo "--subpath universe-base" {};
   universe-dependent-sum = nixpkgs.haskell.lib.doJailbreak (self.callCabal2nixWithOptions "universe" universeRepo "--subpath universe-dependent-sum" {});
