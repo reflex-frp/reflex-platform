73dd3199cc6e02f7ac3e3b32781ff0dc411e12e1
John Ericson <John.Ericson@Obsidian.Systems>
2020-01-02 19:17:14 -0500

    More systematically CI profiled builds

--- release.nix
+++ release.nix
@@ -29,10 +29,7 @@ let
   perPlatform = lib.genAttrs cacheBuildSystems (system: let
     getRP = args: import ./. ((self-args // { inherit system; }) // args);
     reflex-platform = getRP {};
-    reflex-platform-profiled = getRP { enableLibraryProfiling = true; };
     reflex-platform-nojsstring = getRP { useTextJSString = false; };
-    otherDeps = getOtherDeps reflex-platform;
-    skeleton-test = import ./skeleton-test.nix { inherit reflex-platform; };
 
     jsexeHydra = exe: exe.overrideAttrs (attrs: {
       postInstall = ''
@@ -54,42 +51,53 @@ let
       // (lib.optionalAttrs reflex-platform.androidSupport reflex-platform.ghcAndroidAarch64._dep)
       // benchmark.dep
       ;
-  in {
+
+    optDebugVariants = [
+      "unprofiled"
+      "profiled"
+    ];
+    perOptDebugVariant = lib.genAttrs optDebugVariants (variant: let
+      reflex-platform = getRP { enableLibraryProfiling = variant == "profiled"; };
+      skeleton-test = import ./skeleton-test.nix { inherit reflex-platform; };
+      otherDeps = getOtherDeps reflex-platform;
+      collect = v:
+        if lib.isDerivation v then [v]
+        else if lib.isAttrs v then lib.concatMap collect (builtins.attrValues v)
+        else if lib.isList v then lib.concatMap collect v
+        else [];
+      packages = {
+        tryReflexShell = reflex-platform.tryReflexShell;
+        ghc.ReflexTodomvc = reflex-platform.ghc.reflex-todomvc;
+        ghc8_6.reflexTodomvc = reflex-platform.ghc8_6.reflex-todomvc;
+        skeleton-test-ghc = skeleton-test.ghc;
+        skeleton-test-ghcjs = skeleton-test.ghcjs;
+      } // lib.optionalAttrs (reflex-platform.androidSupport) {
+        inherit (reflex-platform) androidReflexTodomvc;
+        inherit (reflex-platform) androidReflexTodomvc-8_6;
+        skeleton-test-project-android = skeleton-test.project.android;
+      } // lib.optionalAttrs (reflex-platform.iosSupport) {
+        inherit (reflex-platform) iosReflexTodomvc;
+        inherit (reflex-platform) iosReflexTodomvc-8_6;
+        skeleton-test-project-ios = skeleton-test.project.ios;
+      } // drvListToAttrs otherDeps
+        // drvListToAttrs (lib.filter lib.isDerivation reflex-platform.cachePackages)
+      ;
+    in packages // {
+      cache = reflex-platform.pinBuildInputs "reflex-platform-${system}-${variant}"
+        (collect packages ++ reflex-platform.cachePackages ++ otherDeps);
+    });
+  in perOptDebugVariant // {
     inherit dep;
-    tryReflexShell = reflex-platform.tryReflexShell;
     ghcjs.reflexTodomvc = jsexeHydra reflex-platform.ghcjs.reflex-todomvc;
     # Doesn't currently build. Removing from CI until fixed.
     ghcjs8_6.reflexTodomvc = jsexeHydra reflex-platform.ghcjs8_6.reflex-todomvc;
-    ghc.ReflexTodomvc = reflex-platform.ghc.reflex-todomvc;
-    ghc8_6.reflexTodomvc = reflex-platform.ghc8_6.reflex-todomvc;
-    profiled = {
-      ghc8_6.reflexTodomvc = reflex-platform-profiled.ghc8_6.reflex-todomvc;
-    } // lib.optionalAttrs (reflex-platform.androidSupport) {
-      inherit (reflex-platform-profiled) androidReflexTodomvc;
-      inherit (reflex-platform-profiled) androidReflexTodomvc-8_6;
-    } // lib.optionalAttrs (reflex-platform.iosSupport) {
-      inherit (reflex-platform-profiled) iosReflexTodomvc;
-      inherit (reflex-platform-profiled) iosReflexTodomvc-8_6;
-    };
     nojsstring = {
       ghcjs.reflexTodomvc = reflex-platform-nojsstring.ghcjs.reflex-todomvc;
     };
-    skeleton-test-ghc = skeleton-test.ghc;
-    skeleton-test-ghcjs = skeleton-test.ghcjs;
     inherit benchmark demoVM;
     cache = reflex-platform.pinBuildInputs "reflex-platform-${system}"
-      (builtins.attrValues dep ++ reflex-platform.cachePackages ++ otherDeps);
-  } // lib.optionalAttrs (reflex-platform.androidSupport) {
-    inherit (reflex-platform) androidReflexTodomvc;
-    inherit (reflex-platform) androidReflexTodomvc-8_6;
-    skeleton-test-project-android = skeleton-test.project.android;
-  } // lib.optionalAttrs (reflex-platform.iosSupport) {
-    inherit (reflex-platform) iosReflexTodomvc;
-    inherit (reflex-platform) iosReflexTodomvc-8_6;
-    skeleton-test-project-ios = skeleton-test.project.ios;
-  } // drvListToAttrs otherDeps
-    // drvListToAttrs (lib.filter lib.isDerivation reflex-platform.cachePackages) # TODO no filter
-  );
+      (builtins.attrValues dep ++ map (a: a.cache) (builtins.attrValues perOptDebugVariant));
+  });
 
   metaCache = local-self.pinBuildInputs "reflex-platform-everywhere"
     (map (a: a.cache) (builtins.attrValues perPlatform));
