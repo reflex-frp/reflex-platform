ebb184ee6aea9eea2eaad6eef574ba8cf9359bbe
John Ericson <John.Ericson@Obsidian.Systems>
2020-01-03 18:08:51 -0500

    Restore bespoke Nixpkgs thunk default.nix
    I am not sure we still want this, but I would rather not change it as
    part of this PR.

--- nixpkgs/default.nix
+++ nixpkgs/default.nix
@@ -1,7 +1,9 @@
-# DO NOT HAND-EDIT THIS FILE
-import ((import <nixpkgs> {}).fetchFromGitHub (
-  let json = builtins.fromJSON (builtins.readFile ./github.json);
-  in { inherit (json) owner repo rev sha256;
-       private = json.private or false;
-     }
-))
+let
+  fetchFromGitHub = { owner, repo, rev, sha256, branch }:
+    if (builtins ? "fetchTarball")
+    then builtins.fetchTarball {
+        inherit sha256;
+        url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz";
+      }
+    else (import <nixpkgs> {}).fetchFromGitHub { inherit owner repo rev; };
+in import (fetchFromGitHub (builtins.fromJSON (builtins.readFile ./github.json)))
