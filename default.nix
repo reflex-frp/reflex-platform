4085c524783e63b50652167626d1bd8734ca40fc
E Cardenas <card@obsidian.systems>
2020-01-27 15:24:36 -0500

    use fromjust hascallstack branch

--- default.nix
+++ default.nix
@@ -196,8 +196,8 @@ let iosSupport = system == "x86_64-darwin";
       bootPkgs = nixpkgsCross.ghcjs.buildPackages.haskell.packages.ghc865;
       ghcjsSrc = fetchgit {
         url = "https://github.com/obsidiansystems/ghcjs.git";
-        rev = "06f81b44c3cc6c7f75e1a5a20d918bad37294b52";
-        sha256 = "02mwkf7aagxqi142gcmq048244apslrr72p568akcab9s0fn2gvy";
+        rev = "e87e27eeb2b8a77886c4a3badf01c6d81ce204c7";
+        sha256 = "02mwkf7aagxqi142gcmq048244apslrr72p568akcab9s0fn2gv0";
         fetchSubmodules = true;
       };
     };
