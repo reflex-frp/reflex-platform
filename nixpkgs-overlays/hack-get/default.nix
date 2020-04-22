5905eaf01f4d2883138162d13164fa85fdcc9de6
Tom Smalley <tfsmalley@gmail.com>
2020-04-22 17:36:44 +0100

    Remove deprecation warning

--- nixpkgs-overlays/hack-get/default.nix
+++ nixpkgs-overlays/hack-get/default.nix
@@ -33,10 +33,7 @@ self:
           || throw "Thunk at ${toString p} has files in addition to ${name} and optionally default.nix and .attr-cache. Remove either ${name} or those other files to continue (check for leftover .git too)."
         else false;
     in
-      if isObeliskThunkWithThunkNix then
-        builtins.trace
-          "DEPRECATED: hackGet is deprecated for thunks containing thunk.nix; Use `import ${p}/thunk.nix` instead of `hackGet ${p}`"
-          (import (p + /thunk.nix))
+      if isObeliskThunkWithThunkNix then import (p + /thunk.nix)
       else if hasValidThunk "git.json" then (
         let gitArgs = filterArgs (builtins.fromJSON (builtins.readFile (p + "/git.json")));
         in if builtins.elem "@" (lib.stringToCharacters gitArgs.url)
