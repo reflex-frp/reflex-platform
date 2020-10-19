4d33a56d655fec875bdf4f3ccd1ae7ff360d1b4d
Ali Abrar <aliabrar@gmail.com>
2020-10-19 06:39:29 -0400

    haskell-overlays: replace uses of callHackageDirect

--- haskell-overlays/untriaged.nix
+++ haskell-overlays/untriaged.nix
@@ -83,96 +83,22 @@ in self: super: {
   http-streams = doJailbreak (self.callHackage "http-streams" "0.8.6.1" {});
 
   # pandoc 2.11 and dependencies
-  commonmark = self.callHackageDirect {
-    pkg = "commonmark";
-    ver = "0.1.0.2";
-    sha256 = "sha256:12r65r3dbqwwbr698zvwjcgbp5r68zzd2w2ycp6iz1lv094l49l9";
-  } {};
-  commonmark-extensions = self.callHackageDirect {
-    pkg = "commonmark-extensions";
-    ver = "0.2.0.1";
-    sha256 = "sha256:13zyf9vqi41bv0cdxr6y2lx9yvq7ca0m0qp4q7jajagd8l6xvqby";
-  } {};
-  commonmark-pandoc = self.callHackageDirect {
-    pkg = "commonmark-pandoc";
-    ver = "0.2.0.1";
-    sha256 = "sha256:0a3has4ijw499rfmcq5ck59s4x73zz56gm1dans96d9qx069pjl0";
-  } {};
-  citeproc = doJailbreak (self.callHackageDirect {
-    pkg = "citeproc";
-    ver = "0.1.0.2";
-    sha256 = "sha256:0ia0ah718icl1ahwchqvq56kl96m8xxmkck2lkn303139s2fxarq";
-  } {});
-  emojis = self.callHackageDirect {
-    pkg = "emojis";
-    ver = "0.1";
-    sha256 = "sha256:0lwj0vm3wfjh437ssfrf5zplpm9a838wi5w170csjjw7chp6qli5";
-  } {};
-  jira-wiki-markup = self.callHackageDirect {
-    pkg = "jira-wiki-markup";
-    ver = "1.3.2";
-    sha256 = "sha256:063v26pdahz2nd6g7w3j5zidf6n3sd1rgns5jv52scky626jrp6j";
-  } {};
-  pandoc = self.callHackageDirect {
-    pkg = "pandoc";
-    ver = "2.11.0.2";
-    sha256 = "sha256:0kpqvm0gf3mws1azh8a6r1kb26khklsawr47gcqpfw599d4bxhz7";
-  } {};
-  pandoc-types = self.callHackageDirect {
-    pkg = "pandoc-types";
-    ver = "1.22";
-    sha256 = "sha256:1ldqp52zrgwdxw67m860g3hkn5zb83cmm0j4cqm36rivgkp3z99c";
-  } {};
-  texmath = self.callHackageDirect {
-    pkg = "texmath";
-    ver = "0.12.0.3";
-    sha256 = "sha256:1abjr6cldsqm6q0v3q2zvy5frhc4hjp6c6h8pf67yqzkplr02av4";
-  } {};
-  rfc5051 = self.callHackageDirect {
-    pkg = "rfc5051";
-    ver = "0.2";
-    sha256 = "sha256:1zv3ksy8crpq30f4agk7j7khx5lljjpjalfwyhaznhyg2ax5bbf8";
-  } {};
-  HsYAML = self.callHackageDirect {
-    pkg = "HsYAML";
-    ver = "0.2.1.0";
-    sha256 = "sha256:0r2034sw633npz7d2i4brljb5q1aham7kjz6r6vfxx8qqb23dwnc";
-  } {};
-  connection = self.callHackageDirect {
-    pkg = "connection";
-    ver = "0.3.1";
-    sha256 = "sha256:0qjdz2fxxszbns7cszhnkwm8x8l3xlnad6iydx2snfi416sypiy0";
-  } {};
-  doclayout = self.callHackageDirect {
-    pkg = "doclayout";
-    ver = "0.3";
-    sha256 = "sha256:1si707v469rc43sxgkp2x27n1bsdj29vb18s4czjbbqc14ri8ng1";
-  } {};
-  doctemplates = self.callHackageDirect {
-    pkg = "doctemplates";
-    ver = "0.8.2";
-    sha256 = "sha256:1idmqpc90b9ifpl1gy3ixi0ny6kqi1vxg033zkyl55fa4jykksn3";
-  } {};
-  haddock-library = doJailbreak (self.callHackageDirect {
-    pkg = "haddock-library";
-    ver = "1.8.0";
-    sha256 = "sha256:1hmfrfygazdkyxxgh2n2a0ff38c8p4bnlxpk9gia90jn0c5im2n5";
-  } {});
-  hslua  = self.callHackageDirect {
-    pkg = "hslua";
-    ver = "1.1.0";
-    sha256 = "sha256:1fph7qa5pfaz3x68qj78s5kccam96abddvc3wsfpcp2ldsz9bh7x";
-  } {};
-  skylighting = self.callHackageDirect {
-    pkg = "skylighting";
-    ver = "0.10.0.2";
-    sha256 = "sha256:1nhsvbmhib8iddsykvlf7rp77qgbx2mk71qh9skhd0454yinwv2n";
-  } {};
-  skylighting-core = self.callHackageDirect {
-    pkg = "skylighting-core";
-    ver = "0.10.0.2";
-    sha256 = "sha256:0a2bgpwf6y54hlrqcy3ry02in6cdik79y69pllsadbdvc7vddcfb";
-  } {};
-
-
+  commonmark = self.callHackage "commonmark" "0.1.0.2" {};
+  commonmark-extensions = self.callHackage "commonmark-extensions" "0.2.0.1" {};
+  commonmark-pandoc = self.callHackage "commonmark-pandoc" "0.2.0.1" {};
+  citeproc = doJailbreak (self.callHackage "citeproc" "0.1.0.2" {});
+  emojis = self.callHackage "emojis" "0.1" {};
+  jira-wiki-markup = self.callHackage "jira-wiki-markup" "1.3.2" {};
+  pandoc = self.callHackage "pandoc" "2.11.0.2" {};
+  pandoc-types = self.callHackage "pandoc-types" "1.22" {};
+  texmath = self.callHackage "texmath" "0.12.0.3" {};
+  rfc5051 = self.callHackage "rfc5051" "0.2" {};
+  HsYAML = self.callHackage "HsYAML" "0.2.1.0" {};
+  connection = self.callHackage "connection" "0.3.1" {};
+  doclayout = self.callHackage "doclayout" "0.3" {};
+  doctemplates = self.callHackage "doctemplates" "0.8.2" {};
+  haddock-library = doJailbreak (self.callHackage "haddock-library" "1.8.0" {});
+  hslua  = self.callHackage "hslua" "1.1.0" {};
+  skylighting = self.callHackage "skylighting" "0.10.0.2" {};
+  skylighting-core = self.callHackage "skylighting-core" "0.10.0.2" {};
 }
