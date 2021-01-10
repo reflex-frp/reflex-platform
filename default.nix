a673a1f11d36182a3be4a1202868adc8f0764bf9
ibzaman <ibizapeanut@gmail.com>
2021-01-09 16:21:32 -0800

    add support for haskell-language-server 1.5.1

--- default.nix
+++ default.nix
@@ -14,6 +14,7 @@
 , haskellOverlaysPre ? []
 , haskellOverlaysPost ? haskellOverlays
 , hideDeprecated ? false # The moral equivalent of "-Wcompat -Werror" for using reflex-platform.
+, hlsSupport ? false
 }:
 
 let iosSupport = system == "x86_64-darwin";
@@ -491,6 +492,10 @@ in let this = rec {
       pkgconfig
       closurecompiler
       ;
+  } // lib.optionalAttrs hlsSupport {
+    haskell-language-server = nixpkgs.haskell.lib.justStaticExecutables (nativeHaskellPackages.override {
+      overrides = nixpkgs.haskell.overlays.haskell-language-server;
+    }).haskell-language-server;
   };
 
   workOn = haskellPackages: package: (overrideCabal package (drv: {
