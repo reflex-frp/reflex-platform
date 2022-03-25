9c8814bfbff42cebd4f2ce1e4af792070f50b049
Anton Gushcha <ncrashed@protonmail.com>
2022-03-25 08:01:52 +0100

    Fix SIGBUS crashes on armv7a

--- nixpkgs-overlays/mobile-ghc/default.nix
+++ nixpkgs-overlays/mobile-ghc/default.nix
@@ -4,6 +4,7 @@ self: super: {
     compiler = super.haskell.compiler // lib.mapAttrs (n: v: v.overrideAttrs (drv: {
       patches = (drv.patches or []) ++ lib.optionals self.stdenv.targetPlatform.useAndroidPrebuilt [
         ./8.6.y/android-patches/force-relocation.patch
+        ./8.6.y/android-patches/strict-align.patch
       ];
     })) { inherit (super.haskell.compiler) ghc865 ghcSplices-8_6 ghc8107 ghcSplices-8_10; };
   };
