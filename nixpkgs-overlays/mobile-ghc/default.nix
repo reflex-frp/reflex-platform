081f0247a3277c7cab2be31e8f94cdb7d71526c7
Anton Gushcha <ncrashed@protonmail.com>
2022-03-28 17:50:49 +0300

    Use align patch only for ghc 8.6.x

--- nixpkgs-overlays/mobile-ghc/default.nix
+++ nixpkgs-overlays/mobile-ghc/default.nix
@@ -1,11 +1,25 @@
 { lib }:
-self: super: {
+let
+  versionWildcard = versionList: let
+    versionListInc = lib.init versionList ++ [ (lib.last versionList + 1) ];
+    bottom = lib.concatStringsSep "." (map toString versionList);
+    top = lib.concatStringsSep "." (map toString versionListInc);
+  in version: lib.versionOlder version top && lib.versionAtLeast version bottom;
+in self: super: {
   haskell = super.haskell // {
     compiler = super.haskell.compiler // lib.mapAttrs (n: v: v.overrideAttrs (drv: {
-      patches = (drv.patches or []) ++ lib.optionals self.stdenv.targetPlatform.useAndroidPrebuilt [
-        ./8.6.y/android-patches/force-relocation.patch
-        ./8.6.y/android-patches/strict-align.patch
-      ];
+      patches = let
+        isAndroid = self.stdenv.targetPlatform.useAndroidPrebuilt;
+        isGhc86x = versionWildcard [ 8 6 ] v.version;
+      in
+        (drv.patches or []) ++
+        lib.optionals isAndroid [
+          ./8.6.y/android-patches/force-relocation.patch
+        ] ++
+        lib.optionals (isAndroid && isGhc86x) [
+          ./8.6.y/android-patches/strict-align.patch
+        ];
     })) { inherit (super.haskell.compiler) ghc865 ghcSplices-8_6 ghc8107 ghcSplices-8_10; };
   };
 }
+
