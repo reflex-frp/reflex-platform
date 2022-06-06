dec99fbd6f55402f1e028132c94bd01c9cbadcea
Amélia Liao <me@amelia.how>
2022-06-06 09:27:56 -0300

    track reflex-dom upstream

--- haskell-overlays/reflex-packages/dep/reflex-dom/thunk.nix
+++ haskell-overlays/reflex-packages/dep/reflex-dom/thunk.nix
@@ -1,14 +1,9 @@
 # DO NOT HAND-EDIT THIS FILE
-let fetch = {url, rev, branch ? null, sha256 ? null, fetchSubmodules ? false, private ? false, ...}:
-  let realUrl = let firstChar = builtins.substring 0 1 url; in
-    if firstChar == "/" then /. + url
-    else if firstChar == "." then ./. + url
-    else url;
-  in if !fetchSubmodules && private then builtins.fetchGit {
-    url = realUrl; inherit rev;
-    ${if branch == null then null else "ref"} = branch;
-  } else (import <nixpkgs> {}).fetchgit {
-    url = realUrl; inherit rev sha256;
+let fetch = { private ? false, fetchSubmodules ? false, owner, repo, rev, sha256, ... }:
+  if !fetchSubmodules && !private then builtins.fetchTarball {
+    url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz"; inherit sha256;
+  } else (import <nixpkgs> {}).fetchFromGitHub {
+    inherit owner repo rev sha256 fetchSubmodules private;
   };
-  json = builtins.fromJSON (builtins.readFile ./git.json);
+  json = builtins.fromJSON (builtins.readFile ./github.json);
 in fetch json
