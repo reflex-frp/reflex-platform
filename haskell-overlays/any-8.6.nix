ad80eb81d47a56ecdd518fe7058dc9a688794a10
cidkidnix <cidkidnix@protonmail.com>
2022-09-12 10:20:59 -0500

    ghc_8_6: Fix cabal2nix & cabal-install
    Needed overrides to have the right Cabal, distribution-nixpkgs, and
    random versions

--- haskell-overlays/any-8.6.nix
+++ haskell-overlays/any-8.6.nix
@@ -10,4 +10,14 @@ self: super: {
   resolv = super.resolv.override {
     base16-bytestring = self.base16-bytestring_0_1_1_7;
   };
+  cabal2nix = super.cabal2nix.override { 
+    distribution-nixpkgs = self.distribution-nixpkgs-8_6;
+  };
+  distribution-nixpkgs-8_6 = super.distribution-nixpkgs_1_6_0.override {
+    Cabal = super.Cabal_3_2_1_0;                                                            
+  };
+  cabal-install = super.cabal-install.override {
+    Cabal = super.Cabal_3_4_0_0;
+    random = super.random_1_2_0;
+  };
 }
