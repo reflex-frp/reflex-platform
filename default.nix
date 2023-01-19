4a03848cf60c208f802409019eb5fd3d271b127a
cidkidnix <cidkidnix@protonmail.com>
2023-01-19 08:09:47 -0600

    More documentation updates

--- default.nix
+++ default.nix
@@ -1,3 +1,4 @@
+# Example usage of this project
 import ./project.nix rec {
   name = "reflex-todomvc";
   src = ../reflex-todomvc;
@@ -17,60 +18,33 @@ import ./project.nix rec {
         repo = "android-activity";
         rev = "2bc40f6f907b27c66428284ee435b86cad38cff8";
         sha256 = "sha256-AIpbe0JZX68lsQB9mpvR7xAIct/vwQAARVHAK0iChV4=";
-	#sha256 = "sha256-CveWAOQDOjhX8hmRzCLImOkaabTytgftM6I7Sya68bM=";
       };
-      #src = (builtins.fetchGit { url = "https://github.com/obsidiansystems/android-activity.git"; rev = "a51bf130b04af92645c040df065c54161e99a335"; 
     }
   ];
   overrides = [
-    #{ packages.reflex.configureFlags = [ "-f-use-template-haskell" ]; }
-    ({ config, lib, pkgs, ... }: {
- /*     packages.${name}.components = pkgs.lib.optionalAttrs (pkgs.stdenv.targetPlatform.isiOS) {
-        library = {
-          depends = [
-            config.hsPkgs.jsaddle-wkwebview
-          ];
-          frameworks = [
-            pkgs.darwin.apple_sdk.frameworks.CoreFoundation
-            pkgs.darwin.apple_sdk.frameworks.Foundation
-            pkgs.darwin.apple_sdk.frameworks.AppKit
-            #pkgs.darwin.apple_sdk.frameworks.UserNotifications
-          ];
-        };
-        exes.reflex-todomvc.frameworks = [
-          #pkgs.darwin.apple_sdk.frameworks.AppKit
-          #pkgs.darwin.apple_sdk.frameworks.WebKit
-          #pkgs.darwin.apple_sdk.frameworks.Foundation
-          #pkgs.darwin.apple_sdk.frameworks.CoreFoundation
-          #pkgs.darwin.apple_sdk.frameworks.Cocoa
-        ];
-      };
-  */
-    })
     ({ config, pkgs, lib, ... }: {
       packages.android-activity.components.library = lib.optionalAttrs (pkgs.stdenv.targetPlatform.isAndroid) {
         depends = [
-	  pkgs.buildPackages.buildPackages.jdk
-	  pkgs.androidndkPkgs_23b.libraries.headers
+	      pkgs.buildPackages.buildPackages.jdk
+	      pkgs.androidndkPkgs_23b.libraries.headers
         ];
         cSources = [
           pkgs.androidndkPkgs_23b.libraries.headers
         ];
-	configureFlags = [ 
-	  "--extra-lib-dirs=${pkgs.androidndkPkgs_23b.libraries.headers}"
-	  "--extra-include-dirs=${pkgs.androidndkPkgs_23b.libraries.headers}"
-	];
+	    configureFlags = [ 
+	      "--extra-lib-dirs=${pkgs.androidndkPkgs_23b.libraries.headers}"
+	      "--extra-include-dirs=${pkgs.androidndkPkgs_23b.libraries.headers}"
+	    ];
       };
       packages.reflex-dom = {
-	flags = {
-	  webkit2gtk = lib.mkForce false;
-	};
+	    flags = {
+	      webkit2gtk = lib.mkForce false;
+	    };
       };
       packages.jsaddle-wkwebview.components.library = {
         configureFlags = pkgs.lib.optionals (pkgs.stdenv.targetPlatform.isiOS) [ "-f-include-app-delegate" ];
         frameworks =
           if (pkgs.stdenv.targetPlatform.isiOS) then lib.mkForce [ pkgs.darwin.iosSdkPkgs.sdk ]
-          #[ pkgs.darwin.apple_sdk.frameworks.AppKit pkgs.darwin.apple_sdk.frameworks.CoreFoundation pkgs.darwin.apple_sdk.frameworks.UserNotifications ] 
           else [ ];
       };
     })
--- project.nix
+++ project.nix
@@ -1,27 +1,25 @@
 # NOTE: Define the interface
-{ name
-, compiler-nix-name ? "ghc8107"
-, src
-, overrides ? [ ]
-, extraSrcFiles ? [ ]
-, setupCross ? true
-, hackageOverlays ? [ ]
-, allowUnfree ? false
-, android_sdk_accept_license ? false
-, nixpkgsArgs ? { }
+{ name # Name of the current project
+, compiler-nix-name ? "ghc8107" # What compiler we should be using
+, src # Source of the current project
+, overrides ? [ ] # Overrides to packages
+, extraSrcFiles ? [ ] # ExtraSrcFiles to include in the project builds
+, setupCross ? true # Setup cross-compiling
+, hackageOverlays ? [ ] # Overlays for hackage, to pass to the cabal solver
+, allowUnfree ? false # Allow Unfree
+, android_sdk_accept_license ? false # Accept android sdk license terms
+, nixpkgsArgs ? { } # Extra nixpkgs arguments
 }:
 let
   # TODO:
   # - Remove this let box properly
   # - Allow for pkgs to be overriden
 
-  # Logic to bootstrap packages
+  # Logic to bootstrap packages that isn't our local checkout
   haskell-nix = import ../haskell.nix { };
   pkgs-pre = import haskell-nix.sources.nixpkgs-unstable (haskell-nix.nixpkgsArgs);
 
-  
-
-  # Patch the packages with some commits
+  # Patch the packages with some commits external to our specific checkout
   remotePatches = [
     {
       url = "https://github.com/obsidiansystems/nixpkgs/commit/d39ee6b7c45deb224d95f717bd1e6e2144e09dd9.diff";
@@ -31,10 +29,6 @@ let
       url = "https://github.com/obsidiansystems/nixpkgs/commit/4516c1a5bb5d11209324bd00239448528bd5fb6d.diff";
       sha256 = "sha256-6GyCvZbuquVS++xR68e+jb4IiFPlIbbJb/kmc9uTers=";
     }
-    #{
-    #  url = "https://github.com/NixOS/nixpkgs/pull/192629.diff";
-    #  sha256 = "sha256-0RF4ocBAXn4y+jmfN72uu5FEFmtG7wKO5QmNZCxZ1ww=";
-    #}
   ];
 
   overlay = (self: super: super.lib.optionalAttrs (super.stdenv.targetPlatform.isAndroid) {
@@ -48,6 +42,7 @@ let
     });
   });
 
+  # Actually patch our nixpkgs
   patchedNixpkgs = pkgs-pre.applyPatches {
     name = "patched-nixpkgs";
     src = ./nixpkgs;
@@ -57,31 +52,20 @@ let
   # Our final packages with the patched commits
   pkgs = import patchedNixpkgs (haskell-nix.nixpkgsArgs // { config.overlays = [ overlay ]; config.android_sdk.accept_license = true; config.allowUnfree = true; } // nixpkgsArgs);
 
-  haskellnixCross = builtins.mapAttrs (a: v: import ../haskell.nix { pkgs = v; }) pkgs.pkgsCross;
-  androidCabal = pkgsrc: pkgs.runCommandNoCC "modify-src" { } ''
-    set -eux
-    mkdir -p $out
-    cp -r ${pkgsrc}/* $out
-    ls -la $out
-    sed -i 's%^executable *\(.*\)$%executable lib\1.so\n  cc-options: -shared\n  ld-options: -shared\n  ghc-options: -shared%i' $out/*.cabal
-  '';
-  x86Cabal = pkgsrc: pkgs.runCommandNoCC "modify-src" { } ''
+  mklibcabal = pkgsrc: pkgs.runCommandNoCC "modify-src" { } ''
     set -eux
     mkdir -p $out
     cp -r ${pkgsrc}/* $out
     ls -la $out
     sed -i 's%^executable *\(.*\)$%executable lib\1.so%i' $out/*.cabal
   '';
-
-  suffixSalt = a: builtins.replaceStrings [ "-" "." ] [ "_" "_" ] a;
 in
 (pkgs.haskell-nix.project' ({
   inherit name compiler-nix-name;
   crossPlatforms = p: [ p.aarch64-android ];
   src = pkgs.haskell-nix.haskellLib.cleanGit {
     inherit name;
-    #inherit src;
-    src = x86Cabal src;
+    src = mklibcabal src;
   };
   modules = [
     { packages."${name}".components = extraSrcFiles; }
@@ -90,27 +74,35 @@ in
         echo "!!! Save Splices $out/lib/haskell.nix/$pname"
         export EXTERNAL_SPLICES_SAVE="$out/lib/haskell.nix/$pname"
       '';
-
     })
   ] ++ overrides;
 })).extend (final: prev: rec {
-  inherit haskellnixCross;
-  # Hackage overlays
-  # Needed if a newer version of a package isn't on the current
-  # index of hackage
+
+  # This constructs a "fake" hackage to pull different packages from
+  # this is used in case that something on proper hackage doesn't have 
+  # the version bounds for packages that we need to properly solve
+  # the current project
+
+  # Outputs:
+  #  buildCommands - commands to build the generatedHackage jsons
+  #  generatedHackage - generated hackage setup
+  #  package-overlays - overlays to setup src properly after the solver has succeeded
+  #  extra-hackage-tarballs - generated tarballs to be passed to the cabal solver
+  #  extra-hackages - alias to (import generatedHackage) - use this in the project'
   hackage-driver = import ./modules/hackage-driver.nix { pkgs = pkgs-pre; modules = hackageOverlays; };
  
   android = (import ./modules/android/default.nix {
     inherit (pkgs) pkgs buildPackages;
     acceptAndroidSdkLicenses = true;
+    # Pass the crossPkgs android-prebuilt package set
     pkg-set = crossSystems.aarch64-android-prebuilt.pkg-set;
   });
 
   app = android.buildApp {
-   package = p: p.reflex-todomvc.components.reflex-todomvc;
-   executableName = "reflex-todomvc";
-   applicationId = "org.reflexfrp.todomvc";
-   displayName = "Reflex TodoMVC";
+    package = p: p.reflex-todomvc.components.reflex-todomvc;
+    executableName = "reflex-todomvc";
+    applicationId = "org.reflexfrp.todomvc";
+    displayName = "Reflex TodoMVC";
   };
 
   # Usage of cross-driver sets up all of the various splices cruft to
@@ -120,14 +112,13 @@ in
       haskell-nix = ../haskell.nix;
       plan-pkgs = import (final.plan-nix);
       inherit name compiler-nix-name;
-      #inherit src;
-      src = androidCabal src;
+      src = mklibcabal src;
       inherit (hackage-driver) extra-hackage-tarballs extra-hackages;
       inherit (final) pkg-set;
-      #plan-pkgs = import (final.plan-nix);
       crossPkgs = v;
       splice-driver = import ./modules/splice-driver.nix { dontSplice = [ "fgl" "Cabal" "android-activity" ]; };
       overrides = [
+        # Move this later, not hacky but should be in android configs specifically
         ({ config, lib, pkgs, ... }: {
           packages.${name} = {
             components.exes = lib.optionalAttrs (pkgs.stdenv.targetPlatform.isAndroid) {
@@ -135,8 +126,6 @@ in
                 hardeningDisable = [ "pie" ];
                 ghcOptions = [
                   "-shared"
-                  "-no-pie"
-                  "-no-pie"
                   "-fPIC"
                   "-threaded"
                   "-no-hs-main"
@@ -144,15 +133,15 @@ in
                   "-lffi"
                   "-lm"
                   "-llog"
-              ];
-              configureFlags = [
-                "--extra-lib-dirs=${pkgs.androidndkPkgs_23b.libraries}"
-                "--ld-options=-shared"
-                "--ld-options=-no-pie"
-                "--ld-options=-Wl,--gc-sections,--version-script=${./exts/android/haskellActivity.version},-u,Java_systems_obsidian_HaskellActivity_haskellStartMain,-u,hs_main"
-              ];
+                ];
+                configureFlags = [
+                  "--extra-lib-dirs=${pkgs.androidndkPkgs_23b.libraries}"
+                  "--ld-options=-shared"
+                  "--ld-options=-no-pie"
+                  "--ld-options=-Wl,--gc-sections,--version-script=${./exts/android/haskellActivity.version},-u,Java_systems_obsidian_HaskellActivity_haskellStartMain,-u,hs_main"
+                ];
+              };
             };
-          };
           };
         })
       ] ++ overrides ++ final.hackage-driver.package-overlays;
