41c634881f77915de8d0996f51b9082e672b2f6a
cidkidnix <cidkidnix@protonmail.com>
2023-01-27 09:40:03 -0600

    Add GHCJS casing to cross-driver

--- modules/cross-driver.nix
+++ modules/cross-driver.nix
@@ -3,19 +3,22 @@
 # We currently use a "splice-driver" to do all of the dirty work regarding setting-up "preBuild"
 # to load splices
 
-{ name, 
-  src, 
-  crossPkgs, 
-  splice-driver, 
-  hardening-driver, 
-  compiler-nix-name, 
-  overrides ? [ ], 
-  pkg-set, 
-  spliced-packages ? pkg-set, 
-  ... 
+# We also generate a new package-set here with "project'" from haskell.nix since passing through the plan-pkgs doesn't always work 100% of the time
+# when building for crossPlatforms
+{ name,
+  src,
+  crossPkgs,
+  splice-driver,
+  hardening-driver,
+  compiler-nix-name,
+  overrides ? [ ],
+  pkg-set,
+  spliced-packages ? pkg-set,
+  ...
 }@args: let
+  # Should probably be an inherit list with a mapping from (isiOS -> isIos)
   filterStdenv = attrs: builtins.listToAttrs (builtins.concatMap (a: if crossPkgs.lib.hasPrefix "is" a then [{ name = if a == "isiOS" then "isIos" else a; value = attrs.${a}; }] else []) (builtins.attrNames attrs));
-  inherit (crossPkgs) lib stdenv; 
+  inherit (crossPkgs) lib stdenv;
 in crossPkgs.haskell-nix.project' {
   inherit name compiler-nix-name;
   # NOTE: The way that haskell.nix resolves the ghcjs compiler is super weird, so default to ghc(js)8107 on any platform
@@ -31,7 +34,6 @@ in crossPkgs.haskell-nix.project' {
   index-state = args.index-state or null;
 
   modules = [
-    ({ config, lib, ... }: { packages.Cabal.patches = lib.mkForce [ ]; })
     # NOTE: Set compiler properly
     ({ config, lib, ... }: {
       config.compiler.nix-name = lib.mkForce (compiler-nix-name);
@@ -41,10 +43,22 @@ in crossPkgs.haskell-nix.project' {
     # currently haskell.nix doesn't do this properly and it ends up in
     # weird cases where some things aren't properly included to be built
     { cabal.system = filterStdenv stdenv.hostPlatform; }
-  ] ++ overrides 
+  ] ++ overrides
+
+    # Disable some stuff to make ghcjs properly function
+    # since haskell.nix enables too strict of rules by default
+    ++ lib.optionals (stdenv.targetPlatform.isGhcjs) [
+      {
+        config.doExactConfig = lib.mkForce false;
+        config.reinstallableLibGhc = lib.mkForce false;
+      }
+    ]
+    ++ lib.optionals (!stdenv.targetPlatform.isGhcjs) ([
+        ({ config, lib, ... }: { packages.Cabal.patches = lib.mkForce [ ]; })
+      ]
     # NOTE: Use the splice driver to setup the loading side of splices
     # refer to ./splice-driver.nix
-    ++ lib.optionals (!stdenv.targetPlatform.isGhcjs) (splice-driver {
+    ++ (splice-driver {
     attrs = pkg-set.config.packages;
     string = (aname: cname: subname:
       if cname == "library" then ''
@@ -56,5 +70,5 @@ in crossPkgs.haskell-nix.project' {
       '');
     }) ++ (hardening-driver {
       attrs = pkg-set.config.packages;
-    });
+    }));
 }
