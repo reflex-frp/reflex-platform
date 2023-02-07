be5ba0b4fdd3e1cbf5e2b412a234c5be82f0efcd
cidkidnix <cidkidnix@protonmail.com>
2023-02-07 16:17:34 -0600

    Update example for overhaul

--- example/default.nix
+++ example/default.nix
@@ -1,30 +1,20 @@
 # Example usage of this project
-let
-  nix-thunk = import ../dep/nix-thunk { };
-in
-import ../project.nix rec {
+{
+  project ? import ../default.nix {
+    android_sdk_accept_license = true;
+    allowUnfree = true;
+  }
+}:
+project ({ pkgs, thunkSource, ... }: {
   name = "reflex-todomvc";
-  patchNixpkgs = true;
-  remotePatches = [
-    {
-      url = "https://github.com/obsidiansystems/nixpkgs/commit/d39ee6b7c45deb224d95f717bd1e6e2144e09dd9.diff";
-      sha256 = "sha256-stn4C43O5M0Qk80gj7YK/87qCDflnm/AwYcOXv5fErI=";
-    }
-    {
-      url = "https://github.com/obsidiansystems/nixpkgs/commit/4516c1a5bb5d11209324bd00239448528bd5fb6d.diff";
-      sha256 = "sha256-6GyCvZbuquVS++xR68e+jb4IiFPlIbbJb/kmc9uTers=";
-    }
-  ];
-  src = nix-thunk.thunkSource ../dep/reflex-todomvc;
+  src = thunkSource ../dep/reflex-todomvc;
   compiler-nix-name = "ghc8107Splices";
-  ghcjs-compiler-nix-name = "ghcjs8107";
-  android_sdk_accept_license = true;
-  allowUnfree = true;
+  ghcjs-compiler-nix-name = pkgs;
   extraSrcFiles = {
     library.extraSrcFiles = [ "style.css" ];
     exes.reflex-todomvc.extraSrcFiles = [ "style.css" ];
   };
-  hackageOverlays = pkgs: [
+  hackageOverlays = [
     #{
     #  name = "android-activity";
     #  version = "0.1.1";
@@ -53,4 +43,4 @@ import ../project.nix rec {
     #({ config, lib, ... }: { packages.reflex-todomvc.src = lib.mkForce src; })
     ({ config, lib, ... }: { packages.bitvec.patches = lib.mkForce [ ]; })
   ];
-}
+})
