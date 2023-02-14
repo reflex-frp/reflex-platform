9f7c52d77ac59a527af307a78a299dcf816fe997
cidkidnix <cidkidnix@protonmail.com>
2023-02-14 11:00:06 -0600

    Move nix code to regular compiler defs

--- default.nix
+++ default.nix
@@ -42,6 +42,8 @@ let iosSupport = system == "x86_64-darwin";
               # nixpkgs-21.05 ships with a version of autoreconf that is incompatible with ghc 8.6.5,
               # Cf. https://gitlab.haskell.org/ghc/ghc/-/commit/ad2ef3a13f1eb000eab8e3d64592373b91a52806
               ./haskell-overlays/splices-load-save/ghc-8.6-autoreconf.patch
+            ] ++ super.lib.optionals (super.stdenv.targetPlatform.isDarwin) [
+              ./haskell-overlays/patches/ghc865/fix-big-sur.patch
             ];
           })).override {
             bootPkgs = super.haskell.packages.ghc865Binary // {
@@ -129,17 +131,6 @@ let iosSupport = system == "x86_64-darwin";
             autoreconfHook = lib.optional self.stdenv.buildPlatform.isDarwin super.autoreconfHook269;
           };
 
-          haskell = super.haskell // {
-            compiler = super.haskell.compiler // super.lib.optionalAttrs (super.stdenv.isDarwin) {
-              ghc865 = super.haskell.compiler.ghc865.overrideAttrs (old: {
-                patches = (old.patches or []) ++ [ ./haskell-overlays/patches/ghc865/fix-big-sur.patch ];
-              });
-
-              ghcSplices-8_6 = super.haskell.compiler.ghcSplices-8_6 (old: {
-                patches = (old.patches or []) ++ [ ./haskell-overlays/patches/ghc865/fix-big-sur.patch ];
-              });
-            };
-          };
           # Bump ios-deploy
           # - for faster deployments
           # - fixes debug deploy with iOS 16/macos 12.3/ xcode 13.4.1
