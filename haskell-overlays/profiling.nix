06d778cdba3483be4d9064f1e56aa190ad5f7c72
cidkidnix <cidkidnix@protonmail.com>
2023-02-15 02:42:57 -0600

    Make disabled profiling packages for ghcjs global

--- haskell-overlays/profiling.nix
+++ haskell-overlays/profiling.nix
@@ -5,6 +5,12 @@
 with haskellLib;
 
 self: super: {
+  optparse-applicative = haskellLib.overrideCabal super.optparse-applicative (drv: {
+    enableLibraryProfiling = false;
+  });
+  th-orphans = haskellLib.overrideCabal super.th-orphans (drv: {
+    enableLibraryProfiling = false;
+  });
 
   # Override mkDerivation to inherit global settings
   mkDerivation = expr: super.mkDerivation (expr // {
