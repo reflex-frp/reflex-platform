813214a13f9911ff300db6d60dee64ca0a4b586f
Ali Abrar <ali.abrar@obsidian.systems>
2023-09-07 12:28:06 -0700

    ghc810: add support for aarch64

--- nixpkgs-overlays/ghc.nix
+++ nixpkgs-overlays/ghc.nix
@@ -1,7 +1,24 @@
 { lib }:
 
 self: super: {
+  cabal2nix-unwrapped = super.haskell.packages.ghc8107.cabal2nix;
   # Apply custom patches to Haskell compilers
   haskell = super.haskell // {
+    compiler = super.haskell.compiler // {
+      ghc8107 = super.haskell.compiler.ghc8107.overrideAttrs (drv: {
+        patches = (drv.patches or []) ++ (super.lib.optionals (super.stdenv.targetPlatform.isDarwin && super.stdenv.targetPlatform.isAarch64) [
+          (super.fetchurl {
+            url = "https://raw.githubusercontent.com/reflex-frp/reflex-platform/mars/modules/patches/aarch64-darwin/fix_dead_strip.patch";
+            sha256 = "sha256-1VcftxpaMmLzMnB8X4M6Xg9o+OmgpaNOeF7Yrn1x0EI=";
+          })
+        ]);
+      });
+    };
+    packages = super.haskell.packages // {
+      ghc8107 = super.haskell.packages.ghc8107.override {
+        buildHaskellPackages = self.buildPackages.haskell.packages.ghc8107;
+        ghc = self.buildPackages.haskell.compiler.ghc8107;
+      };
+    };
   };
 }
