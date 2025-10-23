86bcd242c483052092eb3f2970b845559d03cf26
lazyLambda <galen.sprout@gmail.com>
2025-10-22 22:28:02 -0400

    Bump targetSdkVersion to 35

--- android/build.gradle.nix
+++ android/build.gradle.nix
@@ -70,7 +70,7 @@ android {
     defaultConfig {
         applicationId "${applicationId}"
         minSdkVersion 21
-        targetSdkVersion 34
+        targetSdkVersion 35
         versionCode ${version.code}
         versionName "${version.name}"
         multiDexEnabled false
